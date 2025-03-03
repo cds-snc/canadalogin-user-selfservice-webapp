386f5668619c68c59f32131b2bdf3c1e43573c17
Matthew MacMillan <matthew.macmillan@cds-snc.ca>
2025-03-03 17:04:09 -0500

    Removed tailwindscss as no longer needed. Updated styling to use css from figma.

--- frontend/src/views/Home/HomePage.jsx
+++ frontend/src/views/Home/HomePage.jsx
@@ -6,9 +6,7 @@ import {GcdsContainer} from "@cdssnc/gcds-components-react";
 
 export default function HomePage() {
     const {langHref, currentLang} = getLangValues();
-    //<div className="flex flex-col h-screen justify-between">
-    // <div className="container mx-auto m-auto md:border-1 md:rounded-lg md:border-gray-500 pb-5 pr-5 pl-5 md:pb-5 md:pr-30 md:pl-30 md:pt-10 md:max-w-200 justify-center">
-    //
+
     return (
         <GcdsContainer mainContainer centered>
             <GcdsContainer className="gcds-main">
