fc16c0df28ff1ca8238ad363eb2a9ae0e3614fec
Matthew MacMillan <matthew.macmillan@cds-snc.ca>
2025-03-10 11:02:24 -0400

    82 create page for email collection (#87)
    * Create page for Email collection#82
    
    Added page for Email Collection.
    Added Component for Email collection form.
    Updated Components to use for signing up and logging in to be re-used.
    Added Stories for each new component.
    
    * Create page for Email collection#82
    
    Added page for Email Collection.
    Added Component for Email collection form.
    Updated Components to use for signing up and logging in to be re-used.
    Added Stories for each new component.
    
    * Create page for Email collection#82
    
    Added page for Email Collection.
    Added Component for Email collection form.
    Updated Components to use for signing up and logging in to be re-used.
    Added Stories for each new component.
    
    * Create page for Email collection#82
    
    Added page for Email Collection.
    Added Component for Email collection form.
    Updated Components to use for signing up and logging in to be re-used.
    Added Stories for each new component.
    
    * 82 create page for email collection #87
    
    Corrected sign email to use already component link
    
    * 82 create page for email collection #87
    
    Corrected sign email to use already component link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import {Routes, Route, Navigate} from 'react-router';
 import HomePage from "./views/Home/HomePage";
+import SignUpPage from "./views/SignUp/SignUpPage";
 
 
 function App() {
@@ -7,11 +8,9 @@ function App() {
           <Routes>
               <Route path="/" element={ <HomePage />} />
               <Route path="/:language" element={ <HomePage />} />
+              <Route path="/:language/signup" element={<SignUpPage/>}/>
               <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
       );
 }
-
-
-
 export default App;
---
+++ frontend/src/components/SignUp/SignUpEmail.jsx
@@ -0,0 +1,34 @@
+import {
+    GcdsContainer,
+    GcdsHeading,
+    GcdsText
+} from "@cdssnc/gcds-components-react";
+import {AVAILABLE_LANGUAGES, SERVICES} from "../../utils/constants";
+import {getPageContent} from '../../utils/functions';
+import EmailCollectionForm from "./EmailCollectionForm";
+import AlreadyGc from "../Layout/AlreadyGc.jsx";
+
+
+export default function SignUpEmail({currentLang}) {
+    const pageContentJson = getPageContent(currentLang, "SignUpEmail");
+
+    return (
+        <GcdsContainer className="gcds-content" >
+            <GcdsContainer>
+                <GcdsHeading tag="h1">
+                        {pageContentJson['1']}
+                        <GcdsText marginTop="200" marginBottom="0">
+                            {pageContentJson['2']}
+                            <strong> {currentLang===AVAILABLE_LANGUAGES.fr?pageContentJson['3']+' ':''}{` ${SERVICES[0].title}`}{currentLang===AVAILABLE_LANGUAGES.en?' '+pageContentJson['3']:''}</strong>
+                        </GcdsText>
+                </GcdsHeading>
+                <GcdsHeading tag="h2">
+                    {pageContentJson['4']}
+                </GcdsHeading>
+                <EmailCollectionForm currentLang={currentLang}/>
+            </GcdsContainer>
+            <AlreadyGc currentLang={currentLang}/>
+        </GcdsContainer>
+    )
+}
+
