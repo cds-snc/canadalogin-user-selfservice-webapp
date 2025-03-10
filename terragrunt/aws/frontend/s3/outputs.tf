649b86753ee316622f5eee149ab4f551c708f14f
Mahamoud Mohamoud <Mahamoud.Mohamoud@tbs-sct.gc.ca>
2025-03-10 18:54:55 +0000

    Updated IAM permission to invalidate cloudfront cache

--- terragrunt/aws/frontend/s3/outputs.tf
+++ terragrunt/aws/frontend/s3/outputs.tf
@@ -2,3 +2,9 @@ output "frontend_client_app_s3_bucket_id" {
   description = "value of the frontend subdomain zone id"
   value       = module.frontend_client_app.s3_bucket_id
 }
+
+output "frontend_client_app_cloudfront_distribution_id" {
+  description = "value of the frontend subdomain zone id"
+  value       = module.frontend_client_app.cloudfront_distribution_id
+}
+
