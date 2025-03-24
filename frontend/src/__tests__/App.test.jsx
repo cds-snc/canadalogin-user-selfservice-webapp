edea18d4eaf009952951e41d71d188000127fd9d
Matthew MacMillan <matthew.macmillan@cds-snc.ca>
2025-03-24 14:40:20 -0400

    112 create storyboard and tests for email verification (#189)
    * Create storyboard and tests for email verification#112
    
    Completed routing tests
    Refactored test suite
    Upgraded to most recent version of storybook
    
    * Create storyboard and tests for email verification#112
    
    Adding check for submit and alread GC in email verification test

--- frontend/src/__tests__/App.test.jsx
+++ frontend/src/__tests__/App.test.jsx
@@ -19,6 +19,8 @@ vi.mock(import("@cdssnc/gcds-components-react"), async (importOriginal) => {
         GcdsDetails: vi.fn((props)=><gcds-details aria-label='gcds-details' details-title={props['detailsTitle']}>{props['children']}</gcds-details>),
         GcdsLink: vi.fn((props)=><gcds-link aria-label='gcds-link' href-value={props['href']}>{props['children']}</gcds-link>),
         GcdsFooter: vi.fn((props)=><gcds-footer aria-label='gcds-footer' display-value={props['display']} sub-links={props['subLinks']}/>),
+        GcdsStepper: vi.fn((props)=><gcds-stepper aria-label='gcds-stepper' current-step={props['currentStep']} tags={props['tag']}
+                                                  total-steps={props['totalSteps']}>{props['children']}</gcds-stepper>),
         GcdsHeader: vi.fn((props)=><gcds-header aria-label='gcds-header' lang={props['lang']}  lang-href={props['langHref']}
                                                 signature-variant={props['signature-variant']} skip-to-href={props['skipToHref']}/>)
     }
@@ -26,7 +28,6 @@ vi.mock(import("@cdssnc/gcds-components-react"), async (importOriginal) => {
 describe('Routing Test', () => {
 
     const langHref = {attribute:'lang-href', en:'/'+AVAILABLE_LANGUAGES.en, fr:'/'+AVAILABLE_LANGUAGES.fr}
-    const lang =  {attribute:'lang', en:AVAILABLE_LANGUAGES.en, fr:AVAILABLE_LANGUAGES.fr};
     const subLinks =  {attribute:'sub-links', en:getFooter(AVAILABLE_LANGUAGES.en), fr:getFooter(AVAILABLE_LANGUAGES.fr)};
 
     test("Check home route with no language defined", () => {
@@ -35,7 +36,7 @@ describe('Routing Test', () => {
                 <App />
             </MemoryRouter>,
         )
-        checkHomePageContents(lang.en, engJson["Home"], engJson['Button'], engJson["FirstTimeGc"]);
+        checkHomePageContents(AVAILABLE_LANGUAGES.en, engJson["Home"], engJson['Button'], engJson["FirstTimeGc"]);
     });
 
     test("Check home route with fr language defined", () => {
@@ -45,7 +46,7 @@ describe('Routing Test', () => {
                 <App />
             </MemoryRouter>,
         )
-        checkHomePageContents(lang.fr, frJson["Home"], frJson['Button'], frJson["FirstTimeGc"]);
+        checkHomePageContents(AVAILABLE_LANGUAGES.fr, frJson["Home"], frJson['Button'], frJson["FirstTimeGc"]);
     });
 
     test("Check home route with en language defined", () => {
@@ -55,7 +56,7 @@ describe('Routing Test', () => {
                 <App />
             </MemoryRouter>,
         )
-        checkHomePageContents(lang.en, engJson["Home"], engJson['Button'], engJson["FirstTimeGc"]);
+        checkHomePageContents(AVAILABLE_LANGUAGES.en, engJson["Home"], engJson['Button'], engJson["FirstTimeGc"]);
     });
 
     test("Check sign up route with en language defined", () => {
@@ -65,7 +66,7 @@ describe('Routing Test', () => {
                 <App />
             </MemoryRouter>,
         )
-        checkSignUpPageContents(lang.en, engJson["SignUpEmail"],langHref.fr+NAVIGATION_LINKS.signUp, engJson["EmailCollectionForm"], engJson['Button'], engJson["AlreadyGc"]);
+        checkSignUpPageContents(AVAILABLE_LANGUAGES.en, engJson["SignUpEmail"],langHref.fr+NAVIGATION_LINKS.signUp, engJson["EmailCollectionForm"], engJson['Button'], engJson["AlreadyGc"]);
     });
 
     test("Check sign up route with fr language defined", () => {
@@ -75,9 +76,40 @@ describe('Routing Test', () => {
                 <App />
             </MemoryRouter>,
         )
-        checkSignUpPageContents(lang.fr, frJson["SignUpEmail"],langHref.en+NAVIGATION_LINKS.signUp, frJson["EmailCollectionForm"], frJson['Button'], frJson["AlreadyGc"]);
+        checkSignUpPageContents(AVAILABLE_LANGUAGES.fr, frJson["SignUpEmail"],langHref.en+NAVIGATION_LINKS.signUp, frJson["EmailCollectionForm"], frJson['Button'], frJson["AlreadyGc"]);
     });
 
+    test("Check email verification page route with en language defined", async () => {
+
+        vi.mock("../components/Providers/PrivateRoute.jsx", () => {
+            return {
+                default: (props) => props.children,
+            };
+        });
+
+        render(
+            <MemoryRouter initialEntries={[langHref.en + NAVIGATION_LINKS.verifyEmail]}>
+                <App/>
+            </MemoryRouter>,
+        )
+        checkEmailVerificationPageContents(AVAILABLE_LANGUAGES.en, engJson["EmailVerification"], langHref.fr + NAVIGATION_LINKS.verifyEmail, engJson['Button'], engJson["AlreadyGc"]);
+    });
+
+    test("Check email verification page route with fr language defined", async () => {
+
+        vi.mock("../components/Providers/PrivateRoute.jsx", () => {
+            return {
+                default: (props) => props.children,
+            };
+        });
+
+        render(
+            <MemoryRouter initialEntries={[langHref.fr + NAVIGATION_LINKS.verifyEmail]}>
+                <App/>
+            </MemoryRouter>,
+        )
+        checkEmailVerificationPageContents(AVAILABLE_LANGUAGES.fr, frJson["EmailVerification"], langHref.en + NAVIGATION_LINKS.verifyEmail, frJson['Button'], frJson["AlreadyGc"]);
+    });
 
     afterEach(() => {
         cleanup();
@@ -85,8 +117,8 @@ describe('Routing Test', () => {
 
     function checkHomePageContents(language, pageContentJson, buttonJson, alreadyGcJson) {
 
-        expect(screen.queryByLabelText("gcds-header")).toHaveAttribute(langHref.attribute,langHref.language);
-        expect(screen.queryByLabelText("gcds-header")).toHaveAttribute(lang.attribute,lang.language);
+        const header = screen.queryByLabelText("gcds-header");
+        verifyHeader(header, langHref.language, language);
 
         Object.keys(pageContentJson).forEach(key => {
             if(key==='3')
@@ -97,22 +129,25 @@ describe('Routing Test', () => {
             else if (key==='4')
                 expect(screen.queryByLabelText('gcds-details', {selector: 'gcds-details'})).toHaveAttribute('details-title', pageContentJson['4']);
             else if(key==='8')
+            {
                 expect(screen.queryByLabelText('email', {selector: 'input'})).toHaveAttribute("label",pageContentJson['8']);
+
+            }
             else
                 expect(screen.queryByText(pageContentJson[key])).toBeInTheDocument();
         });
 
         expect(screen.queryByText(buttonJson['submit'])).toBeInTheDocument();
-        Object.keys(alreadyGcJson).forEach(key => expect(screen.queryByText(pageContentJson[key])).toBeInTheDocument());
+        Object.keys(alreadyGcJson).forEach(key => expect(screen.queryByText(alreadyGcJson[key])).toBeInTheDocument());
 
-        expect(screen.queryByLabelText('gcds-link', {selector: 'gcds-link'})).toHaveAttribute('href-value', '/'+language+NAVIGATION_LINKS.signUp);
-        expect(screen.queryByLabelText('gcds-footer', {selector: 'gcds-footer'})).toHaveAttribute(subLinks.attribute,subLinks.language);
+        verifyFooterWithAlreadyLink(screen.queryByLabelText('gcds-link', {selector: 'gcds-link'}), '/'+language+NAVIGATION_LINKS.signUp, subLinks[language]);
+
     }
 
     function checkSignUpPageContents(language, pageContentJson, langLink, formContentJson, buttonJson, alreadyGcJson) {
 
-        expect(screen.queryByLabelText("gcds-header")).toHaveAttribute(langHref.attribute, langLink);
-        expect(screen.queryByLabelText("gcds-header")).toHaveAttribute(lang.attribute,lang.language);
+        const header = screen.queryByLabelText("gcds-header");
+        verifyHeader(header, langLink, language);
 
         Object.keys(pageContentJson).forEach(key => {
             if(key==='3')
@@ -135,7 +170,7 @@ describe('Routing Test', () => {
                 expect(screen.queryByLabelText('gcds-email-fieldset', {selector: 'legend'})).toHaveAttribute("hint", formContentJson['4']);
             else if(key==='6') {
                 let options ='[{"label": "'+formContentJson['6']+'","id": "english", "value": "eng","checked":"true"},{"label": "'+formContentJson['7']+'","id": "french", "value": "fr"}]';
-                if(language===lang.fr)
+                if(language===AVAILABLE_LANGUAGES.fr)
                     options ='[{"label": "'+formContentJson['6']+'","id": "english", "value": "eng"},{"label": "'+formContentJson['7']+'","id": "french", "value": "fr","checked":"true"}]';
 
                 expect(screen.queryByLabelText('language', {selector: 'input'})).toHaveAttribute("type","radio");
@@ -144,9 +179,43 @@ describe('Routing Test', () => {
         });
 
         expect(screen.queryByText(buttonJson['submit'])).toBeInTheDocument();
-        Object.keys(alreadyGcJson).forEach(key => expect(screen.queryByText(pageContentJson[key])).toBeInTheDocument());
+        Object.keys(alreadyGcJson).forEach(key => expect(screen.queryByText(alreadyGcJson[key])).toBeInTheDocument());
 
-        expect(screen.queryByLabelText('gcds-link', {selector: 'gcds-link'})).toHaveAttribute('href-value', langHref.language);
-        expect(screen.queryByLabelText('gcds-footer', {selector: 'gcds-footer'})).toHaveAttribute(subLinks.attribute,subLinks.language);
+        verifyFooterWithAlreadyLink(screen.queryByLabelText('gcds-link', {selector: 'gcds-link'}), langHref[language], subLinks[language]);
+    }
+
+    function checkEmailVerificationPageContents(language, pageContentJson, langLink, buttonJson, alreadyGcJson) {
+
+        const header = screen.queryByLabelText("gcds-header");
+        verifyHeader(header, langLink, language);
+
+        const stepper =screen.queryByLabelText('gcds-stepper', {selector: 'gcds-stepper'});
+        expect(stepper).toHaveAttribute('current-step', '1');
+        expect(stepper).toHaveAttribute('tags', 'h1');
+        expect(stepper).toHaveAttribute('total-steps', '5');
+
+        Object.keys(pageContentJson).forEach(key => {
+            if(key!=='6' && key!=='10' && key !=='11' && key!=='12')
+                expect(screen.queryByText(pageContentJson[key])).toBeInTheDocument();
+        });
+
+        expect(screen.queryByText(buttonJson['submit'])).toBeInTheDocument();
+        Object.keys(alreadyGcJson).forEach(key => expect(screen.queryByText(alreadyGcJson[key])).toBeInTheDocument());
+
+        const links = screen.queryAllByLabelText('gcds-link', {selector: 'gcds-link'});
+        expect(links[0]).toHaveAttribute('href-value', '/'+language+NAVIGATION_LINKS.signUp);
+        verifyFooterWithAlreadyLink(links[1], langHref[language], subLinks[language]);
+    }
+
+    function verifyHeader(header, langLink, language)
+    {
+        expect(header).toHaveAttribute(langHref.attribute, langLink);
+        expect(header).toHaveAttribute('lang',language);
+    }
+
+    function verifyFooterWithAlreadyLink(link, langLink, subLink)
+    {
+        expect(link).toHaveAttribute('href-value', langLink);
+        expect(screen.queryByLabelText('gcds-footer', {selector: 'gcds-footer'})).toHaveAttribute(subLinks.attribute,subLink);
     }
 })
