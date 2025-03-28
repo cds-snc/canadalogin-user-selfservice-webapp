afaeb9ba463aba96a27f425c45ac2ec6bd519646
Matthew MacMillan <matthew.macmillan@cds-snc.ca>
2025-03-28 14:03:55 -0400

    Added a test for server error.

--- frontend/src/stories/Tests/SignUp/SignUpPage.stories.jsx
+++ frontend/src/stories/Tests/SignUp/SignUpPage.stories.jsx
@@ -8,9 +8,9 @@ import config from "../../../config.jsx";
 import {http, HttpResponse} from "msw";
 import {TestDataUserProvider} from "../constants.jsx";
 
-
-
 const serverError =  "value is not a valid email address: There must be something after the @-sign.";
+const engErrorPageJson = getPageContent('en', "Error");
+const frErrorPageJson = getPageContent('fr', "Error");
 
 const errorResponse = {
     "success": false,
@@ -34,10 +34,6 @@ const successResponse = {
         "retries": 4
     }
 }
-
-
-const engErrorPageJson = getPageContent('en', "Error");
-const frErrorPageJson = getPageContent('fr', "Error");
 
 export default {
 
@@ -68,11 +64,11 @@ const EmailTemplateBE = (args) =>   {
     )
 }
 
-
 export const EngWithBadEmailFrontEndTest = BadEmailTemplateFE.bind({});
 export const FrenchWithBadEmail = BadEmailTemplateFE.bind({});
 export const BadEmailBackEndTest = EmailTemplateBE.bind({});
 export const SuccessfulEmailBackEndTest = EmailTemplateBE.bind({});
+export const ServerErrorBackEndTest = EmailTemplateBE.bind({});ServerErrorBackEndTest
 
 EngWithBadEmailFrontEndTest.parameters = {
 
@@ -83,7 +79,6 @@ EngWithBadEmailFrontEndTest.parameters = {
         routing: { path: '/:language'+NAVIGATION_LINKS.signUp }
     })
 }
-
 
 EngWithBadEmailFrontEndTest.play = async ({ canvasElement, step }) => {
 
@@ -135,6 +130,7 @@ FrenchWithBadEmail.play = async ({ canvasElement, step }) => {
     });
 
 }
+
 BadEmailBackEndTest.parameters = {
 
     reactRouter: reactRouterParameters({
@@ -151,7 +147,6 @@ BadEmailBackEndTest.parameters = {
         ],
     },
 }
-
 
 BadEmailBackEndTest.play = async ({ canvasElement, step }) => {
 
@@ -179,8 +174,7 @@ SuccessfulEmailBackEndTest.parameters = {
         location: {
             pathParams: { language: 'en' },
         },
-        routing: { path: '/:language'+NAVIGATION_LINKS.signUp },
-        navigate:{ pathname: "test"}
+        routing: { path: '/:language'+NAVIGATION_LINKS.signUp }
     }),
     msw: {
         handlers: [
@@ -190,7 +184,6 @@ SuccessfulEmailBackEndTest.parameters = {
         ],
     },
 }
-
 
 SuccessfulEmailBackEndTest.play = async ({ canvasElement, step }) => {
 
@@ -206,6 +199,44 @@ SuccessfulEmailBackEndTest.play = async ({ canvasElement, step }) => {
     await step('Verify page was navigated properly.', async () => {
         //need to see if possible to test further
         await expect(canvas.queryByText("404 Not Found")).toBeInTheDocument();
+    });
+
+}
+
+ServerErrorBackEndTest.parameters = {
+
+    reactRouter: reactRouterParameters({
+        location: {
+            pathParams: { language: 'en' },
+        },
+        routing: { path: '/:language'+NAVIGATION_LINKS.signUp }
+    }),
+    msw: {
+        handlers: [
+            http.post(`${config.apiUrl}${SUBMIT_END_POINTS.sendOtpCode}`, () => {
+                return null;
+            }),
+        ],
+    },
+}
+
+ServerErrorBackEndTest.play = async ({ canvasElement, step }) => {
+
+    const canvas = await within(canvasElement);
+    await new Promise((r) => setTimeout(r, 1000));
+
+    await step('Submit form with bad email in English For Back End Error', async () => {
+        await userEvent.click(canvas.queryByRole('button'));
+    });
+
+    await new Promise((r) => setTimeout(r, 3000));
+
+    await step('Verify page was navigated properly.', async () => {
+        //need to see if possible to test further
+        const errorSummary = await canvas.queryByTestId('errorSummary');
+        await expect(errorSummary).toBeInTheDocument();
+        await expect(errorSummary.getAttribute('error-links')).toEqual("{\"#email\": \""+engErrorPageJson['7']+"\"}");
+        await expect(errorSummary).toHaveAttribute("heading", engErrorPageJson['1']);
     });
 
 }
