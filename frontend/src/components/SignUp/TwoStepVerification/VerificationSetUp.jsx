72b7942fcbc1010060fda0004f3c3a3e4f8ce24a
Matthew MacMillan <matthew.macmillan@cds-snc.ca>
2025-03-30 06:04:54 -0400

    Removed Already have a GC account to match Figma

--- frontend/src/components/SignUp/TwoStepVerification/VerificationSetUp.jsx
+++ frontend/src/components/SignUp/TwoStepVerification/VerificationSetUp.jsx
@@ -82,7 +82,6 @@ export default function RegisterVerification({currentLang}) {
                     <SubmitButton currentLang={currentLang}  />
                 </GcdsContainer>
             </GcdsContainer>
-            <AlreadyGc currentLang={currentLang}/>
         </GcdsContainer>
     )
 }
