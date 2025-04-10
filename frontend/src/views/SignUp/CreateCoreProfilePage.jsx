fdfea78880ebc944bb12949dca399cef3b4dbd9b
Matthew MacMillan <matthew.macmillan@cds-snc.ca>
2025-04-10 12:47:17 -0400

    Updated RP link to match Figma Removed unused props from Email Verification Page

--- frontend/src/views/SignUp/CreateCoreProfilePage.jsx
+++ frontend/src/views/SignUp/CreateCoreProfilePage.jsx
@@ -3,13 +3,15 @@ import Header from '../../components/Layout/Header';
 import Footer from '../../components/Layout/Footer';
 import {GcdsContainer} from "@cdssnc/gcds-components-react";
 import CreateCoreProfile from "../../components/SignUp/Profile/CreateCoreProfile.jsx";
+import {useUser} from "../../components/Providers/UserContext.jsx";
 
 export default function CreateCoreProfilePage() {
     const {langHref, currentLang} = getLangValues();
+    const {state} = useUser();
 
     return (
         <div className="mainBody">
-            <Header langHref={langHref} currentLang={currentLang}/>
+            <Header langHref={langHref} currentLang={currentLang} service={state.userData.service}/>
             <GcdsContainer className="gcds-page">
                 <CreateCoreProfile currentLang={currentLang}/>
             </GcdsContainer>
--- frontend/src/views/SignUp/SignUpPage.jsx
+++ frontend/src/views/SignUp/SignUpPage.jsx
@@ -3,13 +3,15 @@ import Header from '../../components/Layout/Header';
 import Footer from '../../components/Layout/Footer';
 import SignUpEmail from "../../components/SignUp/SignUpEmail";
 import {GcdsContainer} from "@cdssnc/gcds-components-react";
+import {useUser} from "../../components/Providers/UserContext.jsx";
 
 export default function SignUpPage() {
     const {langHref, currentLang} = getLangValues();
+    const {state} = useUser();
 
     return (
         <div className="mainBody">
-                <Header langHref={langHref} currentLang={currentLang}/>
+                <Header langHref={langHref} currentLang={currentLang} service={state.userData.service}/>
                 <GcdsContainer className="gcds-page">
                     <SignUpEmail currentLang={currentLang}/>
                 </GcdsContainer>
