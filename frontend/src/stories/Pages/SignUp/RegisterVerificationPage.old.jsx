5fd8c98d4730066877ada383b3dcb67053c09230
Matthew MacMillan <matthew.macmillan@cds-snc.ca>
2025-04-17 12:45:19 -0400

    Frontend: Fix ESlint errors and warning#314
    Resolved eslint errors/warnings
    Refactored Storybook pages

---
+++ frontend/src/stories/Pages/SignUp/RegisterVerificationPage.old.jsx
@@ -0,0 +1,46 @@
+// import { withRouter, reactRouterParameters } from 'storybook-addon-remix-react-router';
+// import {NAVIGATION_LINKS} from "../../../utils/constants.jsx";
+// import {UserProvider} from "../../../components/Providers/UserContext.jsx";
+// import RegisterVerificationPage from "../../../views/SignUp/RegisterVerificationPage.jsx";
+//
+//
+//
+// export default {
+//     title: 'GC Sign In/Pages/Sign Up/Two Step Verification Page',
+//     component: RegisterVerificationPage,
+//     decorators: [withRouter,
+//         (Story) => (
+//             <UserProvider>
+//                 <Story />
+//             </UserProvider>
+//         )
+//     ],
+//     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+//     tags: ['autodocs'],
+//
+// };
+//
+// export const English = {
+//     parameters: {
+//         reactRouter: reactRouterParameters({
+//             location: {
+//                 pathParams: { language: 'en' },
+//             },
+//             routing: { path: '/:language'+NAVIGATION_LINKS.verifyEmail }
+//         }),
+//     }
+// };
+//
+// export const French = {
+//     parameters: {
+//         reactRouter: reactRouterParameters({
+//             location: {
+//                 pathParams: { language: 'fr' },
+//             },
+//             routing: { path: '/:language'+NAVIGATION_LINKS.verifyEmail }
+//         }),
+//     }
+//
+//
+// };
+//
