44084a47c09fbce1ff91baa3ac5aea20bae54c00
aaron-johnson-cds-snc-ca <aaron.johnson@cds-snc.ca>
2025-05-15 13:08:20 -0300

    styling and linting fixes

--- backend/app/otp/services/retrieve_transient_otp.py
+++ backend/app/otp/services/retrieve_transient_otp.py
@@ -13,11 +13,16 @@ from app.utils.schemas import ResponseModel
 
 logger = logging.getLogger(__name__)
 
-async def handle_otp_status_retrieval(retrieval_data: RetrievalData, global_http_client: AsyncClient):
+
+async def handle_otp_status_retrieval(
+    retrieval_data: RetrievalData, global_http_client: AsyncClient
+):
     try:
         logger.info(f"Attempting to retrieve {retrieval_data.otpType} OTP.")
         start_time = datetime.now()
-        http_client_response = await dispatch_otp_status_retrieval(retrieval_data, global_http_client)
+        http_client_response = await dispatch_otp_status_retrieval(
+            retrieval_data, global_http_client
+        )
         duration = (datetime.now() - start_time).total_seconds()
         logger.info(
             f"{retrieval_data.otpType} OTP retrieval request response received in {duration:.2f} seconds"
@@ -58,7 +63,9 @@ async def handle_otp_status_retrieval(retrieval_data: RetrievalData, global_http
         )
 
 
-async def dispatch_otp_status_retrieval(retrieval_data: RetrievalData, global_http_client: AsyncClient):
+async def dispatch_otp_status_retrieval(
+    retrieval_data: RetrievalData, global_http_client: AsyncClient
+):
     try:
         access_token = await get_admin_token()
         headers = get_auth_request_headers(access_token, True)
@@ -66,24 +73,32 @@ async def dispatch_otp_status_retrieval(retrieval_data: RetrievalData, global_ht
 
         if retrieval_data.otpType == OtpType.SMS:
             send_transient_otp_url = f"{settings.IBM_VERIFY_TENANT_URL}/v2.0/factors/smsotp/transient/verifications/{retrieval_data.trxnId}"
-            response = await global_http_client.get(send_transient_otp_url, headers=headers)
+            response = await global_http_client.get(
+                send_transient_otp_url, headers=headers
+            )
             return response
 
         elif retrieval_data.otpType == OtpType.VOICE:
             send_transient_otp_url = f"{settings.IBM_VERIFY_TENANT_URL}/v2.0/factors/voiceotp/transient/verifications/{retrieval_data.trxnId}"
-            response = await global_http_client.get(send_transient_otp_url, headers=headers)
+            response = await global_http_client.get(
+                send_transient_otp_url, headers=headers
+            )
             return response
 
         elif retrieval_data.otpType == OtpType.EMAIL:
             send_transient_otp_url = f"{settings.IBM_VERIFY_TENANT_URL}/v2.0/factors/emailotp/transient/verifications/{retrieval_data.trxnId}"
-            response = await global_http_client.get(send_transient_otp_url, headers=headers)
+            response = await global_http_client.get(
+                send_transient_otp_url, headers=headers
+            )
             return response
 
         else:
             generate_error_response(400, "Unknown error")
 
     except HTTPException as he:
-        logger.error(f"HTTP Exception in {retrieval_data.otpType} OTP status check: {str(he)}")
+        logger.error(
+            f"HTTP Exception in {retrieval_data.otpType} OTP status check: {str(he)}"
+        )
         raise he
     except Exception as error:
         logger.error(
