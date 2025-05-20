fed5b63d200cb9555a4b739db97a0ee3faa54fd6
ywcdsdev <yan.wan@cds-snc.ca>
2025-05-20 15:04:28 -0400

    New endpoint, include the branch 440 cache reverse (#444)
    * New endpoint, include the branch 440 cache reverse
    
    * Follow correct way to set notification
    
    * Also, add preferred language operation
    
    * run make formatting linting
    
    * Fix access token w/ main branch conflicts
    
    * Added new get endpoint and also call get after create is successful
    
    * Data linting and correct handle user not found error
    
    * remove defaulted header

--- backend/app/users/v1_router.py
+++ backend/app/users/v1_router.py
@@ -6,10 +6,15 @@ from app.users.schemas import (
     AuthenticatedUserResponse,
     TwoFactorEnrollmentUserData,
     VerifiedTwofactorEnrollmentResponse,
+    ProfileUserData,
+    ProfileGetUserData,
+    ProfileResponse,
 )
 from app.users.services.create import signup_with_password
 from app.users.services.login import signin_with_password
 from app.users.services.two_factor_enrollment import handle_enrolling_user_into_2fa
+from app.users.services.profile import create_profile, get_profile
+
 import logging
 from fastapi import Request
 
@@ -62,3 +67,25 @@ async def user_2fa_enroll(
     return await handle_enrolling_user_into_2fa(
         user_enrollment_data, request.app.state.request_client
     )
+
+
+@router.post(
+    "/profile",
+    response_model=ProfileResponse,
+    tags=["Users"],
+    summary="Create user profile in verify",
+    description="",
+)
+async def user_create_profile(user_data: ProfileUserData, request: Request):
+    return await create_profile(user_data, request.app.state.request_client)
+
+
+@router.get(
+    "/profile",
+    response_model=ProfileResponse,
+    tags=["Users"],
+    summary="Get a single user's profile",
+    description="",
+)
+async def user_get_profile(user_data: ProfileGetUserData, request: Request):
+    return await get_profile(user_data, request.app.state.request_client)
