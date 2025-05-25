a873e54eb9d688702d49278e5bedf19bcd064373
vikramnimmala <vikram.nimmala@cds-snc.ca>
2025-05-25 18:13:24 -0400

    formatted with enjson

--- frontend/src/components/PersonalInfo/ProfileNameEdit.jsx
+++ frontend/src/components/PersonalInfo/ProfileNameEdit.jsx
@@ -1,62 +1,61 @@
 import React from 'react';
-import { GcdsButton, GcdsContainer, GcdsHeading, GcdsInput } from '@cdssnc/gcds-components-react';
 import {
-    GcdsGrid,
-    GcdsText,
+    GcdsButton,
+    GcdsContainer,
+    GcdsHeading,
+    GcdsInput,
     GcdsDetails,
-    GcdsLink
+    GcdsText
 } from '@cdssnc/gcds-components-react';
+import { useParams } from 'react-router';
 import { getPageContent } from '../../utils/functions';
 import { PAGES } from '../../utils/constants';
-import { useParams } from 'react-router';
 import SubmitButton from '../Layout/SubmitButton';
 
-function ProfileNameEdit() {
+export default function ProfileNameEdit() {
     const { language } = useParams();
     const pageNameEditJson = getPageContent(language, PAGES.ProfileNameEdit);
 
-
     return (
         <GcdsContainer>
-            <GcdsHeading tag="h1">Edit your name</GcdsHeading>
-            <GcdsText>This will update your name with <strong>every service you have shared your name with through GC Sign in.</strong> </GcdsText>
-            <GcdsDetails detailsTitle={pageNameEditJson['1']}>
+            <GcdsHeading tag="h1">
+                {pageNameEditJson['5']}
+            </GcdsHeading>
 
-            </GcdsDetails>
+            <GcdsText>
+                {pageNameEditJson['6']}<strong>
+                    {pageNameEditJson['7']}</strong>
+            </GcdsText>
 
+            <GcdsDetails detailsTitle={pageNameEditJson['1']} />
 
             <form id="form" style={{ marginTop: '38px' }}>
-                <GcdsContainer marginTop="100" marginBottom="0" >
+                <GcdsContainer marginTop="100" marginBottom="0">
                     <GcdsInput
                         inputId="firstName"
                         label={pageNameEditJson['2']}
                         name="firstName"
                         type="text"
-                        //value={state.testData!=null?state.testData.email:email}
                         validateOn="other"
-                        // onGcdsChange={(e) => {validateEmail(e.target.value)}}
-                        // errorMessage={error.errorMsg}
-                        data-testid="firstNname"
+                        data-testid="firstName"
                         lang={language}
-                        required ></GcdsInput>
+                    />
                     <GcdsInput
                         inputId="lastName"
                         label={pageNameEditJson['3']}
                         name="lastName"
                         type="text"
-                        //value={state.testData!=null?state.testData.email:email}
                         validateOn="other"
-                        // onGcdsChange={(e) => {validateEmail(e.target.value)}}
-                        // errorMessage={error.errorMsg}
                         data-testid="lastName"
                         lang={language}
-                         ></GcdsInput>
-                         <SubmitButton currentLang={language} disabled={false}/>
-                         <GcdsButton style={{marginLeft: "10px"}} buttonRole='secondary' >{pageNameEditJson['4']}</GcdsButton>
+                        required
+                    />
+                    <SubmitButton currentLang={language} disabled={false} />{' '}
+                    <GcdsButton buttonRole="secondary">
+                        {pageNameEditJson['4']}
+                    </GcdsButton>
                 </GcdsContainer>
             </form>
         </GcdsContainer>
     );
 }
-
-export default ProfileNameEdit;
