d5e1b9131b853719970f3689d461f58b18c59445
vikramnimmala <vikram.nimmala@cds-snc.ca>
2025-06-07 22:13:40 -0400

    Updated UI changes

--- frontend/src/components/Manage/AreYouSureUpdateYourEmail.jsx
+++ frontend/src/components/Manage/AreYouSureUpdateYourEmail.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import {
   GcdsContainer,
   GcdsHeading,
+  GcdsText,
+  GcdsGrid,
+  GcdsButton
 } from "@cdssnc/gcds-components-react";
 import { getPageContent } from "../../utils/functions";
 import { PAGES } from "../../utils/constants";
@@ -11,8 +14,24 @@ export default function AreYouSureUpdateYourEmail() {
   const { language } = useParams();
   const pageContentJson = getPageContent(language, PAGES.areYouSureUpdateYourEmail);
   return (
-    <GcdsContainer>
-      <GcdsHeading tag="h1">{pageContentJson["1"]}</GcdsHeading>
-    </GcdsContainer>
+      <GcdsContainer>
+          <GcdsHeading tag="h1">{pageContentJson["1"]}</GcdsHeading>
+          <GcdsText>
+              {pageContentJson["2"]} <strong>test.user@gmail.com</strong>.
+          </GcdsText>
+          <GcdsText>{pageContentJson["3"]}</GcdsText>
+          <ul>
+              <li>{pageContentJson["4"]}</li>
+          </ul>
+          <br /> &nbsp;
+          <GcdsGrid columns="auto auto" gap="1rem" align-items="center">
+              <GcdsButton>
+                  {pageContentJson["5"]}
+              </GcdsButton>
+              <GcdsButton buttonRole="secondary">
+                  {pageContentJson["6"]}
+              </GcdsButton>
+          </GcdsGrid>
+      </GcdsContainer>
   );
 }
