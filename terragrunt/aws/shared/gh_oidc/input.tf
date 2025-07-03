97b45ccc64499b039cfcfd77b5d23451248e012d
Mahamoud Mohamoud <136502496+MahamoudTBS@users.noreply.github.com>
2025-07-03 13:18:41 -0400

    removed terragrunt from the main folder (#558)
    * removed terragrunt from the main folder
    
    * removed the terragrunt.zip as thats already included int he main zip

--- terragrunt/aws/shared/gh_oidc/input.tf
+++
@@ -1,9 +0,0 @@
-variable "frontend_client_app_s3_bucket_id" {
-  description = "S3 bucket name of Frontend Client app"
-  type        = string
-}
-
-variable "frontend_client_app_cloudfront_distribution_id" {
-  description = "Cloudfront distribution id of Frontend Client app"
-  type        = string
-}
