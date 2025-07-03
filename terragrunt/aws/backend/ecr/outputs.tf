13eb88f374b3fcfc998cc53b18ba687c5db3ca68
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2025-07-03 15:56:04 +0000

    removed terragrunt from the main folder

--- terragrunt/aws/backend/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_url" {
-  description = "The URL of the repository"
-  value       = aws_ecr_repository.backend_ecr.repository_url
-}
-
-output "repository_arn" {
-  description = "The ARN of the repository"
-  value       = aws_ecr_repository.backend_ecr.arn
-} 
--- terragrunt/aws/backend/secrets/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "secret_arn" {
-  description = "ARN of the secret"
-  value       = aws_secretsmanager_secret.ibm_verify.arn
-} 
