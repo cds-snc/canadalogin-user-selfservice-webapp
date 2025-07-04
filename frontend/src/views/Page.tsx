07fc6b0eeafd933b21f3aadf777046bc33973fde
Valentin-LyashenkoCDS <valentin.lyashenko@cds-snc.ca>
2025-07-04 12:22:12 -0400

    feat: add security settings page and update routing

--- frontend/src/views/Page.tsx
+++ frontend/src/views/Page.tsx
@@ -27,6 +27,7 @@ import ProfileYouMayUpdateName from "../components/Manage/ProfileYouMayUpdateNam
 import EnterNewPhoneNumber from "../components/Manage/EnterNewPhoneNumber.jsx";
 import YouMayUpdateEmailAtOtherPlaces from "../components/Manage/YouMayUpdateEmailAtOtherPlaces.jsx";
 import AreYouSureUpdateYourEmail from "../components/Manage/AreYouSureUpdateYourEmail.jsx";
+import SecuritySettings from "../components/Manage/SecuritySettings.jsx";
 
 function PageContents({page}:{page:string}) {
     switch(page) {
@@ -109,6 +110,10 @@ function PageContents({page}:{page:string}) {
         case PAGES.areYouSureUpdateYourEmail:
             return (
                 <AreYouSureUpdateYourEmail />
+            );
+        case PAGES.securitySettings:
+            return (
+                <SecuritySettings />
             );
         case "RP":
             return (
