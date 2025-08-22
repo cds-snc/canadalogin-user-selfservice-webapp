31adfa489a8e63fe2ed7f01d569a443cb88e853d
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2025-08-22 18:19:48 +0000

    update to remove sensitive data

--- backend/app/password/services/second_step_update_password.py
+++ backend/app/password/services/second_step_update_password.py
@@ -83,7 +83,7 @@ async def dispatch_password_otp_validator(
         }
 
         logger.info(
-            f"Form data for password reset: {password_resetter_otp_api_endpoint} => {form_data}"
+            f"Form data for dispatch_password_otp_validator: {password_resetter_otp_api_endpoint}"
         )
         response = await global_http_client.post(
             password_resetter_otp_api_endpoint, json=form_data, headers=headers
