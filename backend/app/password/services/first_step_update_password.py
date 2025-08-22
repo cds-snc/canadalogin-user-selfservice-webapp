1c8948418bc9ff699f909dbd9c32d9b18d646cce
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2025-08-22 18:31:49 +0000

    update

--- backend/app/password/services/first_step_update_password.py
+++ backend/app/password/services/first_step_update_password.py
@@ -79,7 +79,7 @@ async def dispatch_password_otp(
             "steps": [{"method": payload.otpMethod.value}],
         }
         logger.info(
-            f"Form data for password reset: {password_resetter_api_endpoint} => {form_data}"
+            f"Form data for password reset: {password_resetter_api_endpoint}"
         )
         response = await global_http_client.post(
             password_resetter_api_endpoint, json=form_data, headers=headers
