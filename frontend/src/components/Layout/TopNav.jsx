5ec26383ce4ea7866a2dfaad4e3b8bf8f1d52143
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2025-09-03 20:33:49 +0000

    Some more work, things are janky in the tablet range right now

--- frontend/src/components/Layout/TopNav.jsx
+++ frontend/src/components/Layout/TopNav.jsx
@@ -1,16 +1,20 @@
 import { GcdsBreadcrumbs, GcdsBreadcrumbsItem, GcdsContainer, GcdsHeader, GcdsNavGroup, GcdsNavLink, GcdsText, GcdsTopNav } from "@cdssnc/gcds-components-react";
-import { useIsMobile } from "../../hooks/useIsMobile";
+import { useBreakpoints } from "../../hooks/useBreakpoints";
 
 export default function TopNav({ currentLang }) {
     console.log(currentLang)
 
-    const isMobile = useIsMobile();
+    const { mobile, tablet } = useBreakpoints();
 
-    if(isMobile) {
+    // Note for nat:
+    // If we do end up using GcdsTopNav in the desktop case, we can pull the GcdsTopNav stuff into some shared thing.
+
+
+    if(mobile || tablet) {
         return (<>
             <GcdsContainer slot="menu">
-                <div className="gcds-nav-name-container">
-                    <div className="gcds-nav-width-spacer">
+                <div className="gcds-top-nav-container">
+                    <div className="gcds-top-nav-width-spacer">
                         <GcdsText marginBottom="0"><strong>GC Sign in</strong></GcdsText>
                     </div>
                 </div>
@@ -33,12 +37,20 @@ export default function TopNav({ currentLang }) {
     // Our desktop design doesn't align with the design system, so we've
     // implemented our own implementation here.
     return (
-        <GcdsContainer className="gcds-nav-name-container" slot="menu">
-            <div className="gcds-nav-name-container">
-            <div className="gcds-nav-width-spacer">
-            <GcdsText className="gcds-nav-name-text" marginBottom="0"><strong>GC Sign in</strong></GcdsText>
-            </div>
-            </div>
-        </GcdsContainer>
+        <GcdsTopNav
+                slot="menu"
+                label="Top navigation"
+                alignment="right"
+                className="gcds-top-nav"
+            >
+                <GcdsNavLink href="#" slot="home">GC Sign in</GcdsNavLink>
+                <GcdsNavGroup open-trigger="Menu" menu-label="Menu">
+                    <GcdsNavLink href="#">GC Sign in account</GcdsNavLink>
+                    <GcdsNavLink href="#">Personal information</GcdsNavLink>
+                    <GcdsNavLink href="#">Security settings</GcdsNavLink>
+                    <GcdsNavLink href="#">Return to foo.com</GcdsNavLink>
+                    <GcdsNavLink href="#">Sign out</GcdsNavLink>
+                </GcdsNavGroup>
+            </GcdsTopNav>
     )
 }
