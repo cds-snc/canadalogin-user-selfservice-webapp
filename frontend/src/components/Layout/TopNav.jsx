3cc7679c7f87d672ccdd556c0d1710fc3acaf6a6
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2025-09-04 16:26:41 -0400

    364 Add top navigation bar and menu (#641)
    * Begin work, mobile is complete and the GC Sign In text is well aligned on desktop
    
    * Some more work, things are janky in the tablet range right now
    
    * Finish up the UI, and add english/french copy to i18n files
    
    * Minor tweaking to colors and removing some unnecessary diff
    
    * Last tweaks, mostly prettier changes
    
    * Fix small bug where nav menu mobile close button was not being translated to french
    
    * Small formatting tweak
    
    * Update mobile breakpoint so both are in terms of em instead of px
    
    * GC Sign-in button at the left of the desktop-view version should go to the root path

---
+++ frontend/src/components/Layout/TopNav.jsx
@@ -0,0 +1,67 @@
+import { GcdsBreadcrumbs, GcdsBreadcrumbsItem, GcdsContainer, GcdsHeader, GcdsNavGroup, GcdsNavLink, GcdsText, GcdsTopNav } from "@cdssnc/gcds-components-react";
+import { useBreakpoints } from "../../hooks/useBreakpoints";
+import { getPageContent } from "../../utils/functions.jsx";
+import { useUser } from "../Providers/useUser";
+
+export default function TopNav({ currentLang }) {
+    const pageContentJson = getPageContent(currentLang, "TopNavBar");
+    const { state } = useUser();
+
+    const relyingPartyLinkName = state.relyingPartyInfo?.linkName;
+    const relyingPartyUrl = state.relyingPartyInfo?.url;
+    const shouldRenderRelyingPartyLink = relyingPartyLinkName && relyingPartyUrl;
+
+    const { mobile, tablet } = useBreakpoints();
+
+    const navLinksJsx = (<>
+        <GcdsNavLink href={`/${currentLang}/`}>{pageContentJson["3"]}</GcdsNavLink>
+        <GcdsNavLink href={`/${currentLang}/profilehome`}>{pageContentJson["4"]}</GcdsNavLink>
+        <GcdsNavLink href={`/${currentLang}/securitysettings`}>{pageContentJson["5"]}</GcdsNavLink>
+        {shouldRenderRelyingPartyLink && (
+            <GcdsNavLink href={relyingPartyUrl}>
+                {pageContentJson["6"] + relyingPartyLinkName}
+            </GcdsNavLink>
+        )}
+        <GcdsNavLink href="#">{pageContentJson["7"]}</GcdsNavLink>
+    </>)
+
+    if(mobile || tablet) {
+        return (<>
+            <GcdsContainer slot="menu">
+                <div className="gcds-top-nav-container">
+                    <div className="gcds-top-nav-width-spacer">
+                        <GcdsText marginBottom="0">
+                            <strong>
+                                {pageContentJson["1"]}
+                            </strong>
+                        </GcdsText>
+                    </div>
+                </div>
+            </GcdsContainer>
+            <GcdsTopNav
+                slot="menu"
+                label="Top navigation"
+                alignment="right"
+                lang={currentLang}
+            >
+                {navLinksJsx}
+            </GcdsTopNav>
+        </>)
+    }
+
+    return (
+        <GcdsTopNav
+            slot="menu"
+            label="Top navigation"
+            alignment="right"
+            className="gcds-top-nav"
+        >
+            <GcdsNavLink href="/" slot="home">
+                {pageContentJson["1"]}
+            </GcdsNavLink>
+            <GcdsNavGroup open-trigger="Menu" menu-label="Menu">
+                {navLinksJsx}
+            </GcdsNavGroup>
+        </GcdsTopNav>
+    )
+}
