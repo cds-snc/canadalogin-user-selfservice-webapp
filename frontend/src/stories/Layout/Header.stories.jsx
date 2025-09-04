bbf04a0ac4fa28b3413e337eead7c09b5760a716
John Phan <john.phan@cds-snc.ca>
2025-09-04 10:36:41 -0700

    Refactor Storybook preview and Header stories to improve structure and add UserProvider context

--- frontend/src/stories/Layout/Header.stories.jsx
+++ frontend/src/stories/Layout/Header.stories.jsx
@@ -1,4 +1,38 @@
 import Header from '../../components/Layout/Header.jsx';
+import { UserProvider } from '../../components/Providers/UserProvider.tsx';
+import { MemoryRouter } from 'react-router';
+
+// Mock initial state for UserProvider
+const mockInitialState = {
+  isLoading: false,
+  userData: {
+    service: 'Test Service',
+    language: 'en',
+    email: null,
+    emailLanguage: null,
+    emailValidated: false,
+    trxnId: null,
+    passwordSubmitted: false,
+    phone: null,
+    stepVerificationSent: false,
+    stepVerified: false,
+    viewPrivacy: false,
+    id: null,
+    otpType: null,
+    passwordValidated: false
+  },
+  userProfile: null,
+  editProfile: null,
+  urlLanguageBeforeEdit: null,
+  cancelProfileEditing: false,
+  relyingPartyInfo: {
+    icon: 'mock-icon.png',
+    id: 'mock-service',
+    linkName: 'Mock Service',
+    url: 'https://example.com'
+  },
+  authenticatedPages: [],
+};
 
 export default {
   title: 'GC Sign In/Layout/Header',
@@ -8,7 +42,16 @@ export default {
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
-  }
+  },
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <UserProvider initial={mockInitialState}>
+          <Story />
+        </UserProvider>
+      </MemoryRouter>
+    ),
+  ],
 };
 
 export const English = {
