5e5e5af62f25a7867bcfcf72a7fd4a320edcfdd3
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2025-09-04 19:33:41 +0000

    Fix small bug where nav menu mobile close button was not being translated to french

--- frontend/src/components/Layout/TopNav.jsx
+++ frontend/src/components/Layout/TopNav.jsx
@@ -38,6 +38,7 @@ export default function TopNav({ currentLang }) {
                 slot="menu"
                 label="Top navigation"
                 alignment="right"
+                lang={currentLang}
             >
                 {navLinksJsx}
             </GcdsTopNav>
