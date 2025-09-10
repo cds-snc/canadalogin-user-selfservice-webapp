4273385d37719babe54f7b064a1ec86cbd014c77
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2025-09-10 20:30:18 +0000

    Add breadcrumb component

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,20 +1,14 @@
 import { GcdsBreadcrumbs, GcdsBreadcrumbsItem, GcdsContainer, GcdsHeader, GcdsNavGroup, GcdsNavLink, GcdsText, GcdsTopNav } from "@cdssnc/gcds-components-react";
-import { useUser } from "../Providers/useUser";
+
 import TopNav from "./TopNav";
+import Breadcrumbs from "./Breadcrumbs";
 
 export default function Header({ langHref, currentLang }) {
-    const { state } = useUser();
-
-    const redirect = `${state.relyingPartyInfo?.url}`;
     return (
         <GcdsContainer className="gcds-header">
             <GcdsHeader langHref={langHref} skipToHref="#" signature-variant={"colour"} lang={currentLang} >
                 <TopNav currentLang={currentLang} />
-                <GcdsText slot="breadcrumb">
-                    <GcdsBreadcrumbs hideCanadaLink>
-                        <GcdsBreadcrumbsItem href={redirect} >{state.relyingPartyInfo?.linkName || ""}</GcdsBreadcrumbsItem>
-                    </GcdsBreadcrumbs>
-                </GcdsText>
+                <Breadcrumbs />
             </GcdsHeader>
         </GcdsContainer>
     );
