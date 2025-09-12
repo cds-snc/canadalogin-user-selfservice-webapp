a6fe0e6008b66fe50b921aec72550dc29530cbef
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2025-09-12 18:39:39 +0000

    bug: update to merge profile data instead of overwriting existing data

--- backend/app/users/schemas.py
+++ backend/app/users/schemas.py
@@ -7,10 +7,21 @@ from pydantic import BaseModel, ConfigDict, EmailStr, Field
 from app.utils.schemas import ResponseModel
 from app.password.schemas import OtpType
 
+SCIM_CORE_USER = "urn:ietf:params:scim:schemas:core:2.0:User"
+SCIM_IBM_USER_EXT = "urn:ietf:params:scim:schemas:extension:ibm:2.0:User"
+SCIM_IBM_NOTIFICATION_EXT = "urn:ietf:params:scim:schemas:extension:ibm:2.0:Notification"
+
 
 class NotifyType(str, Enum):
     EMAIL = "EMAIL"
     NONE = "NONE"
+
+
+class IBMNotifyTypeExtension(BaseModel):
+    notifyType: NotifyType = Field(
+        default=NotifyType.NONE,
+        description="Setting to NONE will not send any notification, Setting the value to EMAIL will send a notification email to the user that the account was created",
+    )
 
 
 class IBMUserCreateResponse(BaseModel):
@@ -87,18 +98,32 @@ class IBMVerifyUserProfileSchema(BaseModel):
     model_config = ConfigDict(validate_by_name=True, validate_by_alias=True)
 
 
-class ProfilePUTData(BaseModel):
-    schemas: List[str] = Field(
-        default=[
-            "urn:ietf:params:scim:schemas:core:2.0:User",
-            "urn:ietf:params:scim:schemas:extension:ibm:2.0:User",
-        ]
-    )
+class UserProfileUpdateRequest(BaseModel):
     preferredLanguage: Optional[str] = None
     name: Optional[UserProfileName] = None
     userName: EmailStr
     phoneNumbers: Optional[List[MetaDataTypeValue]] = None
     model_config = ConfigDict(validate_by_name=True, validate_by_alias=True)
+
+
+class IBMVerifyUpdateUserProfile(IBMVerifyUserProfileSchema):
+    schemas: List[str] = Field(
+        default=[
+            SCIM_CORE_USER,
+            SCIM_IBM_USER_EXT,
+            # SCIM_IBM_NOTIFICATION_EXT
+        ]
+    )
+    # if we want to notify user of profile updates via email, we need to change notifyType to EMAIL
+    # by default, we set it to NONE to avoid sending notification emails on every profile update
+    # Documentation: To turn off email notifications, send the notifications option "urn:ietf:params:scim:schemas:extension:ibm:2.0:Notification": {"notifyType":"NONE"} in the payload.
+    # https://docs.verify.ibm.com/verify/reference/putuser
+    notification: IBMNotifyTypeExtension = Field(
+        default_factory=IBMNotifyTypeExtension,
+        alias=SCIM_IBM_NOTIFICATION_EXT,
+    )
+
+    model_config = ConfigDict(populate_by_name=True)
 
 
 class ProfileResponse(ResponseModel):
--- backend/app/users/services/profile.py
+++ backend/app/users/services/profile.py
@@ -1,33 +1,64 @@
 import logging
 
-from fastapi import HTTPException
+from fastapi import HTTPException, Request
 from httpx import AsyncClient
 from pydantic import ValidationError
 
-from app.users.schemas import IBMVerifyUserProfileSchema, ProfileResponse, ProfilePUTData
+from app.users.schemas import IBMVerifyUserProfileSchema, ProfileResponse, UserProfileUpdateRequest, IBMVerifyUpdateUserProfile
 from app.utils.access_token import get_auth_request_headers
 from app.config import get_configuration
+from backend.app.utils.request_error_handler import RequestErrorHandler
 
 logger = logging.getLogger(__name__)
 
 
-async def update_profile(
-    global_http_client: AsyncClient,
-    user_data: ProfilePUTData,
-    user_access_token,
-    profile_api_endpoint: str,
+def sanitize_user_profile_data(user_data: UserProfileUpdateRequest) -> dict:
+    validate_updated_data = UserProfileUpdateRequest(**user_data.model_dump())  # validation and then turns it into a UserProfileUpdateRequest data object
+    updated_data_dict = validate_updated_data.model_dump(exclude_unset=True, exclude_none=True)
+    return updated_data_dict
+
+
+async def dispatch_update_user_profile(
+    request: Request,
+    user_data: IBMVerifyUpdateUserProfile,
+    user_access_token: str,
 ):
     try:
         headers = get_auth_request_headers(user_access_token)
-        create_request = ProfilePUTData(
-            **user_data.model_dump()
-        )  # validation and then turns it into a ProfilePUTData data object
-        request_json = create_request.model_dump_json(
-            by_alias=True, exclude_unset=True, exclude_none=True
+        response = await request.app.state.request_client.put(
+            request.app.state.config.profile_api_endpoint, content=user_data, headers=headers
         )
-        response = await global_http_client.put(
-            profile_api_endpoint, content=request_json, headers=headers
+        response.raise_for_status()
+        logger.info("updating user profile changes returned successfully")
+        return response
+
+    except Exception as e:
+        logger.error(f"Error dispatching update_user_profile: {str(e)}", exc_info=True)
+        RequestErrorHandler.handle(e)
+
+
+async def update_profile(
+    request: Request,
+    user_data: UserProfileUpdateRequest,
+    user_access_token,
+):
+    try:
+
+        updated_user_data_dict = sanitize_user_profile_data(user_data)
+
+        user_profile = await my_profile(request.app.state.request_client, user_access_token)
+        user_profile_data = user_profile.data.model_dump()
+        updated_user_data_dict.pop("userName", None)  # Prevent changing the userName (email)
+
+        merged_profile = {**user_profile_data, **updated_user_data_dict}
+
+        validate_merged_profile = IBMVerifyUpdateUserProfile(**merged_profile)
+
+        validated_data = validate_merged_profile.model_dump_json(
+            by_alias=True, exclude_none=True
         )
+
+        response = await dispatch_update_user_profile(request, validated_data, user_access_token)
     except ValidationError as e:
         logger.error(f"Validation Error: {e.json()}")
         raise HTTPException(status_code=422, detail="Request data validation error")
