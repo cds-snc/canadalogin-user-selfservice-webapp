8a3da324813a086fbc89731982ea96e20fd4f76b
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2025-09-16 21:53:07 +0000

    removed await for response.json as the operation is not async

--- backend/app/users/services/profile.py
+++ backend/app/users/services/profile.py
@@ -89,12 +89,12 @@ async def update_profile(
         )
         if response.status_code != 200:
             # parse error details safely
-            json_data = await response.json()
+            json_data = response.json()
             error_detail = json_data.get("detail", "Unknown error")
             raise HTTPException(status_code=response.status_code, detail=error_detail)
 
         logger.info("User profile updated successfully.")
-        json_data = await response.json()
+        json_data = response.json()
         response_data = IBMVerifyUserProfileSchema(**json_data)
         return ProfileResponse(
             success=True,
@@ -121,7 +121,7 @@ async def my_profile(global_http_client: AsyncClient, user_access_token: str):
 
     if response.status_code == 200:
         logger.info("User profile retrieved successfully.")
-        json_data = await response.json()
+        json_data = response.json()
         response_data = IBMVerifyUserProfileSchema(**json_data)
         return ProfileResponse(
             success=True,
@@ -133,7 +133,7 @@ async def my_profile(global_http_client: AsyncClient, user_access_token: str):
         if response.status_code == 401:
             raise HTTPException(status_code=401, detail="Not authenticated")
         else:
-            json_data = await response.json()
+            json_data = response.json()
             error_details = json_data.get("detail")
             raise HTTPException(
                 status_code=response.status_code, detail=f"HTTP error, {error_details}"
