ead72436ed65c2962680be091def6b6de3d95dfc
John Phan <john.phan@cds-snc.ca>
2025-09-17 14:25:05 -0700

    Created EnabledBadge and VerifiedBadge components - Updated styling for EnabledBadge component to better align with Figma - Updated styling for VerifiedBadge component to better align with Figma

---
+++ frontend/src/components/Badges/VerifiedBadge.jsx
@@ -0,0 +1,42 @@
+import {
+  GcdsContainer,
+  GcdsIcon,
+} from "@cdssnc/gcds-components-react";
+
+export default function VerifiedBadge({ text }) {
+  return (
+    <GcdsContainer>
+      <div
+        className="verifiedBadge"
+        style={{
+          display: "flex",
+          height: "1.875rem",
+          padding: "0 0.625rem",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "0.3125rem",
+          borderRadius: "0.3125rem",
+          background: "#E6F6EC",
+          width: "fit-content",
+        }}
+      >
+        <GcdsIcon
+          name="checkmark-circle"
+          className="verifiedIcon"
+          size="text"
+        />
+        <span
+          className="verifiedText"
+          style={{
+            color: "#03662A",
+            lineHeight: "1.5rem",
+            margin: 0,
+            padding: 0,
+          }}
+        >
+          {text}
+        </span>
+      </div>
+    </GcdsContainer>
+  );
+}
