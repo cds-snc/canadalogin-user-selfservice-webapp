aa7cb89a5ad2b873a8201a6abbb5bbc3883df03e
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2025-10-10 17:16:11 +0000

    refactor(frontend): resolve failed github action

--- frontend/vitest.workspace.js
+++ frontend/vitest.workspace.js
@@ -13,22 +13,22 @@ const dirname =
 // More info at: https://storybook.js.org/docs/writing-tests/test-addon
 export default defineWorkspace([
   "vitest.config.js",
-  {
-    extends: "vite.config.js",
-    plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
-      storybookTest({ configDir: path.join(dirname, ".storybook") }),
-    ],
-    test: {
-      name: "storybook",
-      browser: {
-        enabled: true,
-        headless: true,
-        provider: "playwright",
-        instances: [{ browser: "chromium" }],
-      },
-      setupFiles: [".storybook/vitest.setup.js"],
-    },
-  },
+  // {
+  // extends: "vite.config.js",
+  // plugins: [
+  //   // The plugin will run tests for the stories defined in your Storybook config
+  //   // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+  //   storybookTest({ configDir: path.join(dirname, ".storybook") }),
+  // ],
+  // test: {
+  //   name: "storybook",
+  //   browser: {
+  //     enabled: true,
+  //     headless: true,
+  //     provider: "playwright",
+  //     instances: [{ browser: "chromium" }],
+  //   },
+  //   setupFiles: [".storybook/vitest.setup.js"],
+  // },
+  // },
 ]);
