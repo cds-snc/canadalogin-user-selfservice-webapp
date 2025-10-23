482c073eb92c83c562fc0ddaf27179f9fb47c49d
John Phan <john.phan@cds-snc.ca>
2025-10-23 11:00:57 -0700

    fix(change-password): remove unused parentPage prop from OTP validation component

--- frontend/src/features/ChangePassword/components/ChangePasswordIndex.jsx
+++ frontend/src/features/ChangePassword/components/ChangePasswordIndex.jsx
@@ -141,7 +141,6 @@ export default function ChangePasswordIndex() {
         onNext={() => {
           setPasswordUpdateStep("otpValidation");
         }}
-        parentPage={PAGES.addMFAPage}
       />
     ),
     otpValidation: (
