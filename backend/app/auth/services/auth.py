09724fdb48fa29154665275562ba2dbe34779c97
cds-yiwei <yiwei.wang@cds-snc.ca>
2025-12-02 10:46:55 -0800

    fix(backend): 906 bugbackend pkce code challenge (#978)
    * manul set pkce
    
    * fmt-python
    
    * add code-veirfier in reauth

--- backend/app/auth/services/auth.py
+++ backend/app/auth/services/auth.py
@@ -164,8 +164,13 @@ async def reauthenticate_user(request: Request, returnToPage: str = "/"):
         # if the user recently logged in, we can set the max age to 15 minutes
         # will reautenticate after max age value
         max_age_in_seconds = 900
+        # manually generate a code_verifier, make sure PKCE is used
+        code_verifier = generate_token(48)  # Generate a secure random code verifier
         return await oauth.verify.authorize_redirect(
-            request, callback_redirect_uri, max_age=max_age_in_seconds
+            request,
+            callback_redirect_uri,
+            code_verifier=code_verifier,
+            max_age=max_age_in_seconds,
         )
     except OAuthError as error:
         logger.exception("Unexpected error during redirect_to_verify")
