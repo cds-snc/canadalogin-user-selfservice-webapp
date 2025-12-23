49d931563af30c7b7a35a0dbf9bc2c75bebe0e7a
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2025-12-23 16:58:29 +0000

    feat(frontend): Display the Release Tag version in the UI

--- frontend/src/components/Layout/RootLayout.jsx
+++ frontend/src/components/Layout/RootLayout.jsx
@@ -5,15 +5,16 @@ import { getLangValues } from "../../utils/functions";
 import { trackPage } from "../../utils/gatag.jsx";
 import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
+import config from "../../config.jsx";
 
-import { GcdsContainer } from "@cdssnc/gcds-components-react";
+import { GcdsContainer, GcdsText } from "@cdssnc/gcds-components-react";
 
 export default function RootLayout() {
   const { pathname } = useLocation();
   const { state: languageState } = useLanguage();
   const { language } = languageState;
   const { langHref } = getLangValues(language, pathname);
-
+  const { releaseTag } = config;
   useEffect(() => {
     trackPage(pathname);
   }, [pathname]);
@@ -24,6 +25,9 @@ export default function RootLayout() {
       <GcdsContainer className="gcds-page">
         <GcdsContainer size="lg" className="gcds-content" id="main-content">
           <Outlet />
+        </GcdsContainer>
+        <GcdsContainer size="lg" className="gcds-content version-text">
+          <GcdsText marginBottom="0">Version: {releaseTag}</GcdsText>
         </GcdsContainer>
       </GcdsContainer>
 
