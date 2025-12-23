7aa40620bd04a26a0e7044b6027a6a91afb86ed7
Mahamoud Mohamoud <136502496+MahamoudTBS@users.noreply.github.com>
2025-12-23 14:56:58 -0500

    1085 add commit sha or version number to frontend (#1086)
    * feat(frontend): Display the Release Tag version in the UI
    
    * feat(frontend): Display Release Version
    
    * feat(frontend): Updated Component Name

--- frontend/src/components/Layout/RootLayout.jsx
+++ frontend/src/components/Layout/RootLayout.jsx
@@ -5,15 +5,29 @@ import { getLangValues } from "../../utils/functions";
 import { trackPage } from "../../utils/gatag.jsx";
 import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
+import config from "../../config.jsx";
 
-import { GcdsContainer } from "@cdssnc/gcds-components-react";
+import { GcdsContainer, GcdsText } from "@cdssnc/gcds-components-react";
+
+const DisplayReleaseTag = () => {
+  const { releaseTag } = config;
+
+  if (!releaseTag) {
+    return null;
+  }
+
+  return (
+    <GcdsContainer size="lg" className="gcds-content version-text">
+      <GcdsText marginBottom="0">Version: {releaseTag}</GcdsText>
+    </GcdsContainer>
+  );
+};
 
 export default function RootLayout() {
   const { pathname } = useLocation();
   const { state: languageState } = useLanguage();
   const { language } = languageState;
   const { langHref } = getLangValues(language, pathname);
-
   useEffect(() => {
     trackPage(pathname);
   }, [pathname]);
@@ -25,6 +39,7 @@ export default function RootLayout() {
         <GcdsContainer size="lg" className="gcds-content" id="main-content">
           <Outlet />
         </GcdsContainer>
+        <DisplayReleaseTag />
       </GcdsContainer>
 
       <Footer currentLang={language} />
