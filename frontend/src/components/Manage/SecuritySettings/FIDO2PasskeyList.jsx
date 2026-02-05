398b96539ec4da66802ecd29e4af2521b8fae3ef
Melody Habb <melody.habbouche@cds-snc.ca>
2026-02-05 11:45:03 -0500

    fix(frontend): Retrieve and display list of FIDO2 passkeys on Security Settings page (#1142)
    * fix: Add SectionCard for Passkeys and Security Keys
    
    * Update feature flag set to entire Passkey section
    
    * Add retrieve and display passkeys + fix UI with Add, Delete and Rename buttons
    
    * Remove console log and comments
    
    * Add error state
    
    * Remove unused gcds imports
    
    * Remove unused import
    
    * Add delete functionality and cleanup logic
    
    * Change button text
    
    * Remove errorcode state
    
    * Fix vite.config.js
    
    * Fix vite.config.js
    
    * Fix errorcode to match pattern of fetchUserOtpPhoneFactors
    
    * Make delete button secondary
    
    * Make only delete need min one requirement
    
    * Remove requirement to have at least one passkey
    
    * Rename flag and guard fetch with feature flag
    
    * Fix use of showFIDO2PasskeyFeature

---
+++ frontend/src/components/Manage/SecuritySettings/FIDO2PasskeyList.jsx
@@ -0,0 +1,52 @@
+import {
+  GcdsButton,
+  GcdsContainer,
+  GcdsText,
+  GcdsGrid,
+} from "@cdssnc/gcds-components-react";
+import { useNavigate, useParams } from "react-router";
+import { PAGES } from "../../../utils/constants.jsx";
+import { getPageContent } from "../../../utils/functions.jsx";
+import { path } from "../../../utils/routeHelpers.js";
+
+export default function FIDO2PasskeyList({ userFIDO2CredentialsData }) {
+  const { language } = useParams();
+  const navigate = useNavigate();
+  const pageContent = getPageContent(language, PAGES.manage2FAVerifications);
+  const deletePasskeyPage = path(PAGES.deleteFIDO2PasskeyPage, {
+    language: language,
+  });
+
+  return userFIDO2CredentialsData.map(({ id, nickname, created }) => {
+    return (
+      <GcdsContainer key={id}>
+        <GcdsText>
+          <strong>{`${nickname}`}</strong>
+        </GcdsText>
+        <GcdsText>
+          <strong>{pageContent["16"]}</strong>
+          {new Date(created).toLocaleString()}
+        </GcdsText>
+        <GcdsGrid columns="max-content max-content" gap="200">
+          <GcdsButton
+            id="rename-fido2-button"
+            buttonRole="secondary"
+            onGcdsClick={() => {}}
+          >
+            {pageContent["14"]}
+          </GcdsButton>
+          <GcdsButton
+            id="delete-fido2-button"
+            buttonRole="secondary"
+            onClick={() => {
+              navigate(`${deletePasskeyPage}/${id}`);
+            }}
+          >
+            {pageContent["13"]}
+          </GcdsButton>
+        </GcdsGrid>
+        <div className="separator" />
+      </GcdsContainer>
+    );
+  });
+}
--- frontend/src/components/Manage/SecuritySettings/Manage2FAVerifications.jsx
+++ frontend/src/components/Manage/SecuritySettings/Manage2FAVerifications.jsx
@@ -2,8 +2,6 @@ import {
   GcdsButton,
   GcdsContainer,
   GcdsHeading,
-  GcdsLink,
-  GcdsNotice,
   GcdsText,
 } from "@cdssnc/gcds-components-react";
 import { useEffect, useState } from "react";
@@ -17,6 +15,8 @@ import { useUser } from "../../Providers/useUser.js";
 import NoticeFactory from "../../InfoBlocks/NoticeFactory.jsx";
 import config from "../../../config.jsx";
 import PhoneFactorsList from "./PhoneFactorsList.jsx";
+import FIDO2PasskeyList from "./FIDO2PasskeyList.jsx";
+import { fido2Api } from "../../../features/ManageFIDO2/api/fido2Api.jsx";
 
 export default function Manage2FAVerifications() {
   const { language } = useParams();
@@ -25,10 +25,11 @@ export default function Manage2FAVerifications() {
   const navigate = useNavigate();
   const { state, _dispatch } = useUser();
   const [userPhoneFactorsMap, setUserPhoneFactorsMap] = useState({});
+  const [userFIDO2CredentialsData, setUserFIDO2CredentialsData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   // Only show add passkey link in dev and test environments
-  const showAddPasskeyLink =
+  const showFIDO2PasskeyFeature =
     config.environment === VITE_ENVIRONMENTS.dev ||
     config.environment === VITE_ENVIRONMENTS.test;
 
@@ -74,7 +75,33 @@ export default function Manage2FAVerifications() {
       }
     };
 
+    /**
+     * Fetch user's FIDO2 credentials
+     */
+    const fetchUserFIDO2Credentials = async () => {
+      setLoading(true);
+
+      try {
+        const response = await fido2Api.getUserFIDO2Credentials();
+        console.log(response);
+        if (response && response?.data?.authenticated) {
+          setUserFIDO2CredentialsData(response?.data?.credentials || []);
+        }
+      } catch (err) {
+        if (err && err?.data && err?.data?.message) {
+          console.error("err", err);
+        } else {
+          navigate(backToSecuritySettingsPage);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchUserOtpPhoneFactors();
+    if (showFIDO2PasskeyFeature) {
+      fetchUserFIDO2Credentials();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -141,16 +168,54 @@ export default function Manage2FAVerifications() {
         </GcdsButton>
       </GcdsContainer>
 
-      {showAddPasskeyLink && (
-        <GcdsButton
-          id="add-fido2-button"
-          onGcdsClick={(ev) => {
-            ev.preventDefault();
-            navigate(addFido2PagePath);
-          }}
-        >
-          {pageContent["12"]}
-        </GcdsButton>
+      {showFIDO2PasskeyFeature && (
+        <GcdsContainer className="sectionCard">
+          <GcdsHeading
+            tag="h3"
+            marginTop="300"
+            style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
+          >
+            <div
+              style={{
+                display: "flex",
+                height: "1.875rem",
+                padding: "0 0.625rem",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "0.3125rem",
+                borderRadius: "0.3125rem",
+                width: "fit-content",
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="25"
+                viewBox="0 0 16 25"
+                fill="none"
+              >
+                <path
+                  d="M2.18182 24.5464C1.58182 24.5464 1.06818 24.3328 0.640909 23.9055C0.213636 23.4782 0 22.9646 0 22.3646V2.7282C0 2.1282 0.213636 1.61457 0.640909 1.1873C1.06818 0.760023 1.58182 0.546387 2.18182 0.546387H13.0909C13.6909 0.546387 14.2045 0.760023 14.6318 1.1873C15.0591 1.61457 15.2727 2.1282 15.2727 2.7282V22.3646C15.2727 22.9646 15.0591 23.4782 14.6318 23.9055C14.2045 24.3328 13.6909 24.5464 13.0909 24.5464H2.18182ZM2.18182 21.2737V22.3646H13.0909V21.2737H2.18182ZM2.18182 19.0918H13.0909V6.00093H2.18182V19.0918ZM2.18182 3.81911H13.0909V2.7282H2.18182V3.81911Z"
+                  fill="#333333"
+                />
+              </svg>
+              {pageContent["15"]}
+            </div>
+          </GcdsHeading>
+          <FIDO2PasskeyList
+            userFIDO2CredentialsData={userFIDO2CredentialsData}
+          />
+
+          <GcdsButton
+            id="add-fido2-button"
+            onGcdsClick={(ev) => {
+              ev.preventDefault();
+              navigate(addFido2PagePath);
+            }}
+          >
+            {pageContent["12"]}
+          </GcdsButton>
+        </GcdsContainer>
       )}
     </GcdsContainer>
   );
