c7f83a984df4b5f6afc2e3d3a33ad0c2175b046a
Justin Hollick <justin.hollick@cds-snc.ca>
2026-02-16 15:36:43 -0500

    revert vit changes and remove .env.test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "app.auth.signin-connexion.cdssandbox.xyz",
-    port: 3000,
-    allowedHosts: ["app.auth.signin-connexion.cdssandbox.xyz"],
-    https: {
-      key: fs.readFileSync(path.resolve("../backend/certs/key.pem")),
-      cert: fs.readFileSync(path.resolve("../backend/certs/cert.pem")),
-    },
-  },
 });
