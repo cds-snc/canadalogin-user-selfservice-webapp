90b0ed740b5dea2a7a207b825c23e85c0b0a7889
John Phan <john.phan@cds-snc.ca>
2026-02-20 09:10:37 -0800

    feat(tests): Add pytest configuration for environment variables

---
+++ backend/tests/conftest.py
@@ -0,0 +1,9 @@
+import os
+
+
+def pytest_configure():
+    os.environ["IBM_VERIFY_TENANT_URL"] = "https://example.com"
+    os.environ["IBM_VERIFY_PROFILE_MANAGEMENT_API_CLIENT_ID"] = "client-id"
+    os.environ["IBM_VERIFY_PROFILE_MANAGEMENT_API_SECRET"] = "secret"
+    os.environ["IBM_VERIFY_PROFILE_MANAGEMENT_CLIENT_ID"] = "client-id-2"
+    os.environ["IBM_VERIFY_PROFILE_MANAGEMENT_SECRET"] = "secret-2"
