48605d5655ef17d66450b1587d14cd8e53b02cab
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2026-02-23 16:57:26 +0000

    Refactor API files to use centralized axios instance with custom header set

--- frontend/src/features/MFAPhoneNumber/DeleteMFAPhoneNumber/api/DeleteMFAPhoneNumberAPI.jsx
+++ frontend/src/features/MFAPhoneNumber/DeleteMFAPhoneNumber/api/DeleteMFAPhoneNumberAPI.jsx
@@ -1,9 +1,7 @@
-import axios from "axios";
+import axios from "../../../../utils/axiosInstance.js";
 import config from "../../../../config.jsx";
 import { handleApiError } from "../../../../utils/apiErrorHandler.js";
 import { SUBMIT_END_POINTS } from "../../../../utils/constants.jsx";
-
-axios.defaults.withCredentials = true;
 
 export const deleteMFAPhoneNumberApi = {
   // Delete an MFA OTP phone number with OTP verification
