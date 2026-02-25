e74ff3a1dff08fc716ab06da4e9617e17d43d420
John Phan <john.phan@cds-snc.ca>
2026-02-25 13:50:41 -0800

    feat: add server configuration for local development in vitest workspace

--- frontend/vitest.workspace.js
+++ frontend/vitest.workspace.js
@@ -15,6 +15,10 @@ export default defineWorkspace([
   "vitest.config.js",
   {
     extends: "vite.config.js",
+    server: {
+      https: false,
+      host: "localhost",
+    },
     plugins: [
       // The plugin will run tests for the stories defined in your Storybook config
       // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
