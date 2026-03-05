307781f3413bc36789e41cc2577a2bd551b30152
John Phan <john.phan@cds-snc.ca>
2026-03-05 11:50:06 -0800

    refactor(frontend): remove unused verification component and related code

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -3,10 +3,7 @@ import { Navigate } from "react-router";
 import RootLayout from "./components/Layout/RootLayout.jsx";
 import { AppLanguageSetup } from "./components/Providers/AppLanguageSetup";
 import { LanguageProvider } from "./components/Providers/LanguageProvider";
-import {
-  PrivateRoute,
-  StepupPrivateRoute,
-} from "./components/Providers/PrivateRoute.jsx";
+import { PrivateRoute } from "./components/Providers/PrivateRoute.jsx";
 import { UserProvider } from "./components/Providers/UserProvider";
 
 import EditLanguagePreferencePage from "./features/LanguagePreference/components/EditLanguagePreferencePage.jsx";
@@ -15,7 +12,6 @@ import ProfileHome from "./components/Manage/ProfileHome.jsx";
 import Manage2FAVerifications from "./components/Manage/SecuritySettings/Manage2FAVerifications.jsx";
 import SecuritySettings from "./components/Manage/SecuritySettings/SecuritySettings.jsx";
 import EditProfileNamePage from "./features/ProfileName/components/EditProfileNamePage.jsx";
-import Verification from "./components/Verification/Verification.jsx";
 import ChangePasswordIndex from "./features/ChangePassword/components/ChangePasswordIndex.jsx";
 import EditContactPhoneNumberPage from "./features/ContactPhoneNumber/components/EditContactPhoneNumberPage.jsx";
 import AddMFAPage from "./features/MFAPhoneNumber/AddMFAPhoneNumber/component/AddMFAPage.jsx";
@@ -125,11 +121,6 @@ export const appRoutes = [
                     ],
                   },
                 ],
-              },
-              {
-                path: "verification",
-                element: <Verification />,
-                handle: { id: PAGES.verification },
               },
             ],
           },
