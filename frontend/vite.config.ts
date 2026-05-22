73398f0f4b967c55ba88aeac77a25c0c2ece7b08
John Phan <john.phan@cds-snc.ca>
2026-05-22 10:32:47 -0700

    chore: remove server configuration from Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgr()],
-  server: {
-    host: "app.cds-gcsignin-dev.verify.ibm.com",
-    port: 3000,
-    allowedHosts: ["app.cds-gcsignin-dev.verify.ibm.com"],
-    https: {
-      key: fs.readFileSync(path.resolve("../backend/certs/key.pem")),
-      cert: fs.readFileSync(path.resolve("../backend/certs/cert.pem")),
-    },
-  },
 });
