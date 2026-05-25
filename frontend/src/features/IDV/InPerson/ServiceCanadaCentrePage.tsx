0a713475fd9d26f724d67614f38a355bac6319bd
John Phan <john.phan@cds-snc.ca>
2026-05-25 10:17:58 -0700

    feat: update Service Canada and Provincial Verification pages for improved user guidance and localization

--- frontend/src/features/IDV/InPerson/ServiceCanadaCentrePage.tsx
+++ frontend/src/features/IDV/InPerson/ServiceCanadaCentrePage.tsx
@@ -25,95 +25,89 @@ export default function ServiceCanadaCentrePage() {
   }
 
   return (
-    DEV_ONLY_FEATURE && (
-      <GcdsContainer role="main">
-        <GcdsGrid columns="1" gap="450">
-          <GcdsContainer>
-            {" "}
-            <GcdsText marginBottom="0" size="small">
-              {t("ServiceCanadaCentre.pageTitle")}
-            </GcdsText>
-            <GcdsHeading tag="h1">
-              {t("ServiceCanadaCentre.heading")}
-            </GcdsHeading>
-          </GcdsContainer>
+    <GcdsContainer role="main">
+      <GcdsGrid columns="1" gap="450">
+        <GcdsContainer>
+          {" "}
+          <GcdsText marginBottom="0" size="small">
+            {t("ServiceCanadaCentre.pageTitle")}
+          </GcdsText>
+          <GcdsHeading tag="h1">{t("ServiceCanadaCentre.heading")}</GcdsHeading>
+        </GcdsContainer>
 
-          <GcdsContainer>
-            {" "}
-            <GcdsText>
-              <strong>{t("ServiceCanadaCentre.followSteps")}</strong>
-            </GcdsText>
-            <ol>
-              <li>
-                <GcdsText marginBottom="0">
-                  {t("ServiceCanadaCentre.step1")}
-                </GcdsText>
-              </li>
-              <li>
-                <GcdsText marginBottom="0">
-                  {t("ServiceCanadaCentre.step2")}
-                </GcdsText>
+        <GcdsContainer>
+          {" "}
+          <GcdsText>
+            <strong>{t("ServiceCanadaCentre.followSteps")}</strong>
+          </GcdsText>
+          <ol>
+            <li>
+              <GcdsText marginBottom="0">
+                {t("ServiceCanadaCentre.step1")}
+              </GcdsText>
+            </li>
+            <li>
+              <GcdsText marginBottom="0">
+                {t("ServiceCanadaCentre.step2")}
+              </GcdsText>
 
-                <GcdsDetails detailsTitle={t("ServiceCanadaCentre.listOfIds")}>
-                  {t("ServiceCanadaCentre.listOfIds")}
-                </GcdsDetails>
-              </li>
-              <li>
-                <GcdsText marginBottom="0">
-                  {t("ServiceCanadaCentre.step3")}
-                </GcdsText>
-              </li>
-            </ol>
-          </GcdsContainer>
+              <GcdsDetails detailsTitle={t("ServiceCanadaCentre.listOfIds")}>
+                {t("ServiceCanadaCentre.listOfIds")}
+              </GcdsDetails>
+            </li>
+            <li>
+              <GcdsText marginBottom="0">
+                {t("ServiceCanadaCentre.step3")}
+              </GcdsText>
+            </li>
+          </ol>
+        </GcdsContainer>
 
-          <GcdsHeading tag="h2" marginTop="0">
-            {t("ServiceCanadaCentre.receiveCodeHeading")}
-          </GcdsHeading>
+        <GcdsHeading tag="h2" marginTop="0">
+          {t("ServiceCanadaCentre.receiveCodeHeading")}
+        </GcdsHeading>
 
-          <GcdsContainer>
-            {" "}
-            <GcdsText>
-              {t("ServiceCanadaCentre.receiveCodeDescription")}
-            </GcdsText>
-            <GcdsText>
-              {t("ServiceCanadaCentre.emailInstructions")}{" "}
-              <strong>{email}</strong>
-            </GcdsText>
-          </GcdsContainer>
+        <GcdsContainer>
+          {" "}
+          <GcdsText>{t("ServiceCanadaCentre.receiveCodeDescription")}</GcdsText>
+          <GcdsText>
+            {t("ServiceCanadaCentre.emailInstructions")}{" "}
+            <strong>{email}</strong>
+          </GcdsText>
+        </GcdsContainer>
 
-          <GcdsGrid columns="max-content max-content" gap="200">
-            <GcdsButton
-              type="button"
-              onClick={() => {
-                // TODO: navigate to next IDV step
-              }}
-            >
-              {t("ServiceCanadaCentre.continueButton")}
-            </GcdsButton>
-            <GcdsButton
-              type="button"
-              buttonRole="secondary"
-              onClick={() => {
-                navigate(-1);
-              }}
-            >
-              {t("ServiceCanadaCentre.backButton")}
-            </GcdsButton>
-          </GcdsGrid>
-          <GcdsNotice
-            noticeRole="info"
-            noticeTitleTag="h2"
-            noticeTitle={t("ServiceCanadaCentre.moreInfoTitle")}
+        <GcdsGrid columns="max-content max-content" gap="200">
+          <GcdsButton
+            type="button"
+            onClick={() => {
+              // TODO: navigate to next IDV step
+            }}
           >
-            {
-              //TODO: populate with real URL once available
-            }
-            <GcdsLink href={"#"} external={true}>
-              {t("ServiceCanadaCentre.learnMoreLink")}
-            </GcdsLink>
-          </GcdsNotice>
+            {t("ServiceCanadaCentre.continueButton")}
+          </GcdsButton>
+          <GcdsButton
+            type="button"
+            buttonRole="secondary"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            {t("ServiceCanadaCentre.backButton")}
+          </GcdsButton>
         </GcdsGrid>
-      </GcdsContainer>
-    )
+        <GcdsNotice
+          noticeRole="info"
+          noticeTitleTag="h2"
+          noticeTitle={t("ServiceCanadaCentre.moreInfoTitle")}
+        >
+          {
+            //TODO: populate with real URL once available
+          }
+          <GcdsLink href={"#"} external={true}>
+            {t("ServiceCanadaCentre.learnMoreLink")}
+          </GcdsLink>
+        </GcdsNotice>
+      </GcdsGrid>
+    </GcdsContainer>
   );
 }
--- frontend/src/features/IDV/Online/ProvincialVerificationPage.tsx
+++ frontend/src/features/IDV/Online/ProvincialVerificationPage.tsx
@@ -23,86 +23,84 @@ export default function ProvincialVerificationPage() {
   }
 
   return (
-    DEV_ONLY_FEATURE && (
-      <GcdsContainer role="main">
-        <GcdsGrid columns="1" gap="450">
-          <GcdsContainer>
-            <GcdsText marginBottom="0" size="small">
-              {t("ProvincialVerification.pageTitle")}
-            </GcdsText>
-            <GcdsHeading tag="h1">
-              {t("ProvincialVerification.heading")}
-            </GcdsHeading>
-          </GcdsContainer>
+    <GcdsContainer role="main">
+      <GcdsGrid columns="1" gap="450">
+        <GcdsContainer>
+          <GcdsText marginBottom="0" size="small">
+            {t("ProvincialVerification.pageTitle")}
+          </GcdsText>
+          <GcdsHeading tag="h1">
+            {t("ProvincialVerification.heading")}
+          </GcdsHeading>
+        </GcdsContainer>
 
-          <GcdsContainer>
-            <GcdsText>
-              <strong>{t("ProvincialVerification.followSteps")}</strong>
-            </GcdsText>
-            <ol>
-              <li>
-                <GcdsText marginBottom="0">
-                  {t("ProvincialVerification.step1")}
-                </GcdsText>
-              </li>
-              <li>
-                <GcdsText marginBottom="0">
-                  {t("ProvincialVerification.step2")}
-                </GcdsText>
-              </li>
-              <li>
-                <GcdsText marginBottom="0">
-                  {t("ProvincialVerification.step3")}
-                </GcdsText>
-              </li>
-            </ol>
-          </GcdsContainer>
+        <GcdsContainer>
+          <GcdsText>
+            <strong>{t("ProvincialVerification.followSteps")}</strong>
+          </GcdsText>
+          <ol>
+            <li>
+              <GcdsText marginBottom="0">
+                {t("ProvincialVerification.step1")}
+              </GcdsText>
+            </li>
+            <li>
+              <GcdsText marginBottom="0">
+                {t("ProvincialVerification.step2")}
+              </GcdsText>
+            </li>
+            <li>
+              <GcdsText marginBottom="0">
+                {t("ProvincialVerification.step3")}
+              </GcdsText>
+            </li>
+          </ol>
+        </GcdsContainer>
 
-          <GcdsGrid columns="1" gap="300">
-            <GcdsCard
-              cardTitle={t("ProvincialVerification.bcServicesCard")}
-              cardTitleTag="h3"
-              href="#"
-              imgSrc={imgBcServicesCard}
-              imgAlt="British Columbia Logo"
-              // TODO: Replace href with BC Services Card OAuth URL when available
-            ></GcdsCard>
-            <GcdsCard
-              cardTitle={t("ProvincialVerification.albertaAccount")}
-              cardTitleTag="h3"
-              href="#"
-              imgSrc={imgAlbertaAccount}
-              imgAlt="Alberta Logo"
-              // TODO: Replace href with Alberta.ca Account OAuth URL when available
-            ></GcdsCard>
-          </GcdsGrid>
-
-          <GcdsGrid columns="max-content" gap="200">
-            <GcdsButton
-              type="button"
-              buttonRole="secondary"
-              onClick={() => {
-                navigate(-1);
-              }}
-            >
-              {t("ProvincialVerification.backButton")}
-            </GcdsButton>
-          </GcdsGrid>
-
-          <GcdsNotice
-            noticeRole="info"
-            noticeTitleTag="h2"
-            noticeTitle={t("ProvincialVerification.moreInfoTitle")}
-          >
-            {
-              // TODO: populate with real URL once available
-            }
-            <GcdsLink href={"#"} external={true}>
-              {t("ProvincialVerification.learnMoreLink")}
-            </GcdsLink>
-          </GcdsNotice>
+        <GcdsGrid columns="1" gap="300">
+          <GcdsCard
+            cardTitle={t("ProvincialVerification.bcServicesCard")}
+            cardTitleTag="h3"
+            href="#"
+            imgSrc={imgBcServicesCard}
+            imgAlt="British Columbia Logo"
+            // TODO: Replace href with BC Services Card OAuth URL when available
+          ></GcdsCard>
+          <GcdsCard
+            cardTitle={t("ProvincialVerification.albertaAccount")}
+            cardTitleTag="h3"
+            href="#"
+            imgSrc={imgAlbertaAccount}
+            imgAlt="Alberta Logo"
+            // TODO: Replace href with Alberta.ca Account OAuth URL when available
+          ></GcdsCard>
         </GcdsGrid>
-      </GcdsContainer>
-    )
+
+        <GcdsGrid columns="max-content" gap="200">
+          <GcdsButton
+            type="button"
+            buttonRole="secondary"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            {t("ProvincialVerification.backButton")}
+          </GcdsButton>
+        </GcdsGrid>
+
+        <GcdsNotice
+          noticeRole="info"
+          noticeTitleTag="h2"
+          noticeTitle={t("ProvincialVerification.moreInfoTitle")}
+        >
+          {
+            // TODO: populate with real URL once available
+          }
+          <GcdsLink href={"#"} external={true}>
+            {t("ProvincialVerification.learnMoreLink")}
+          </GcdsLink>
+        </GcdsNotice>
+      </GcdsGrid>
+    </GcdsContainer>
   );
 }
