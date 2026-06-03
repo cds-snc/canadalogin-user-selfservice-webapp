0da24c1b87f6353191875671f2980e29298facd5
amalungo <afonso.malungo@cds-snc.ca>
2026-06-03 21:12:30 +0000

    fix: Updated content

---
+++ frontend/src/features/IDV/Online/MobileCompleteConfirmation.tsx
@@ -0,0 +1,61 @@
+import {
+  GcdsButton,
+  GcdsDetails,
+  GcdsGrid,
+  GcdsLink,
+  GcdsNotice,
+  GcdsText,
+  GcdsContainer,
+  GcdsHeading,
+} from "@gcds-core/components-react";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router";
+import { DEV_ONLY_FEATURE } from "../../../utils/constants";
+
+export default function MobileCompleteConfirmation() {
+  const navigate = useNavigate();
+  const { t } = useTranslation("idv");
+
+  if (!DEV_ONLY_FEATURE) {
+    return null;
+  }
+
+  return (
+    <GcdsContainer role="main">
+      <GcdsGrid columns="1" gap="450">
+        <GcdsContainer>
+          <GcdsHeading tag="h1">
+            {t("MobileCompleteConfirmation.heading")}
+          </GcdsHeading>
+        </GcdsContainer>
+
+        <GcdsContainer>
+          <GcdsNotice
+            noticeRole="info"
+            noticeTitleTag="h2"
+            noticeTitle={t("MobileCompleteConfirmation.moreInfoTitle")}
+          >
+            {
+              // TODO: populate with real URL once available
+            }
+            <GcdsText>{t("MobileCompleteConfirmation.moreInfoBody")}</GcdsText>
+            <GcdsLink href={"#"} external={false}>
+              {t("MobileCompleteConfirmation.learnMoreLink")}
+            </GcdsLink>
+          </GcdsNotice>
+        </GcdsContainer>
+
+        <GcdsGrid columns="max-content max-content" gap="200">
+          <GcdsButton
+            type="button"
+            onClick={() => {
+              // TODO: navigate to next IDV step
+            }}
+          >
+            {t("MobileCompleteConfirmation.continueButton")}
+          </GcdsButton>
+        </GcdsGrid>
+      </GcdsGrid>
+    </GcdsContainer>
+  );
+}
