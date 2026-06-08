646344b89ff0fe868de94219ddfbea0254b91ee2
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2026-06-08 21:59:01 +0000

    update lint

--- frontend/src/features/IDV/__tests__/StartIdentityProofingPage.test.jsx
+++ frontend/src/features/IDV/__tests__/StartIdentityProofingPage.test.jsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom/vitest";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import StartIdentityProofingPage from "../StartIdentityProofingPage";
 
