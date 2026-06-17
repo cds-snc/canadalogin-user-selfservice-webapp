870e8a156d6a7e7866580912b6adbc60a8b192c2
John Phan <john.phan@cds-snc.ca>
2026-06-17 12:47:02 -0700

    feat: enhance Service Canada Centre page with ID input fields and improved instructions

--- frontend/src/features/IDV/InPerson/ServiceCanadaCentrePage.tsx
+++ frontend/src/features/IDV/InPerson/ServiceCanadaCentrePage.tsx
@@ -7,6 +7,8 @@ import {
   GcdsText,
   GcdsContainer,
   GcdsSelect,
+  GcdsDateInput,
+  GcdsInput,
 } from "@gcds-core/components-react";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router";
@@ -29,80 +31,138 @@ export default function ServiceCanadaCentrePage() {
 
   return (
     <GcdsContainer role="main">
-      <GcdsGrid columns="1">
-        <GcdsContainer>
-          <GcdsHeading tag="h1">{t("ServiceCanadaCentre.heading")}</GcdsHeading>
-        </GcdsContainer>
+      <GcdsContainer>
+        <GcdsHeading tag="h1">{t("ServiceCanadaCentre.heading")}</GcdsHeading>
+      </GcdsContainer>
 
-        <GcdsContainer>
-          {" "}
-          <GcdsText>
-            <strong>{t("ServiceCanadaCentre.followSteps")}</strong>
-          </GcdsText>
-          <ol>
-            <li>
-              <GcdsText marginBottom="0">
-                {t("ServiceCanadaCentre.step1")}
-              </GcdsText>
-            </li>
-            <li>
-              <GcdsText marginBottom="0">
-                {t("ServiceCanadaCentre.step2")}
-              </GcdsText>
-            </li>
-            <li>
-              <GcdsText marginBottom="0">
-                {t("ServiceCanadaCentre.step3")}
-              </GcdsText>
-            </li>
-          </ol>
-        </GcdsContainer>
+      <GcdsContainer>
+        {" "}
+        <GcdsText>
+          <strong>{t("ServiceCanadaCentre.followSteps")}</strong>
+        </GcdsText>
+        <ol>
+          <li>
+            <GcdsText marginBottom="0">
+              {t("ServiceCanadaCentre.step1")}
+            </GcdsText>
+          </li>
+          <li>
+            <GcdsText marginBottom="0">
+              {t("ServiceCanadaCentre.step2")}
+            </GcdsText>
+          </li>
+          <li>
+            <GcdsText marginBottom="0">
+              {t("ServiceCanadaCentre.step3")}
+            </GcdsText>
+          </li>
+        </ol>
+      </GcdsContainer>
 
-        <GcdsHeading tag="h2" marginTop="0" marginBottom="0">
-          {t("ServiceCanadaCentre.receiveCodeHeading")}
-        </GcdsHeading>
+      <GcdsHeading tag="h2" marginBottom="0">
+        {t("ServiceCanadaCentre.enterDetailsHeading")}
+      </GcdsHeading>
 
+      <form>
         <GcdsSelect
           label={t("ServiceCanadaCentre.selectIdLabel")}
           name="selectId"
           selectId="selectId"
           defaultValue={t("ServiceCanadaCentre.selectIdDropdownDefaultValue")}
+          required
         >
-          <option value="1">{t("ServiceCanadaCentre.selectIdOption1")}</option>
+          <option value="driverLicence">
+            {t("ServiceCanadaCentre.driverLicenceOption")}
+          </option>
+          <option value="photoIDHealthCard">
+            {t("ServiceCanadaCentre.photoIDHealthCardOption")}
+          </option>
+          <option value="photoIDServiceCard">
+            {t("ServiceCanadaCentre.photoIDServiceCardOption")}
+          </option>
+          <option value="passport">
+            {t("ServiceCanadaCentre.passportOption")}
+          </option>
+          <option value="canadianPRCard">
+            {t("ServiceCanadaCentre.canadianPRCardOption")}
+          </option>
+          <option value="indianStatus">
+            {t("ServiceCanadaCentre.indianStatusOption")}
+          </option>
         </GcdsSelect>
 
-        <GcdsGrid columns="max-content max-content" gap="200">
-          <GcdsButton
-            type="button"
-            onClick={() => {
-              navigate(serviceCanadaCodePage);
-            }}
-          >
-            {t("ServiceCanadaCentre.continueButton")}
-          </GcdsButton>
-          <GcdsButton
-            type="button"
-            buttonRole="secondary"
-            onClick={() => {
-              navigate(-1);
-            }}
-          >
-            {t("ServiceCanadaCentre.backButton")}
-          </GcdsButton>
-        </GcdsGrid>
-        <GcdsNotice
-          noticeRole="info"
-          noticeTitleTag="h2"
-          noticeTitle={t("ServiceCanadaCentre.moreInfoTitle")}
+        <GcdsDateInput
+          legend={t("ServiceCanadaCentre.idExpirationLabel")}
+          name="id-expiration-date-input"
+          format="full"
+          required
+        />
+
+        <GcdsHeading tag="h2" marginBottom="300">
+          {t("ServiceCanadaCentre.enterDetailsIdHeading")}
+        </GcdsHeading>
+
+        <GcdsInput
+          required
+          inputId="first-name-input"
+          label={t("ServiceCanadaCentre.firstNameLabel")}
+        />
+        <GcdsInput
+          required
+          inputId="last-name-input"
+          label={t("ServiceCanadaCentre.lastNameLabel")}
+        />
+
+        <GcdsDateInput
+          legend={t("ServiceCanadaCentre.dateOfBirthdayLabel")}
+          name="date-of-birth-input"
+          format="full"
+          required
+        />
+
+        <GcdsInput
+          inputId="address-input"
+          label={t("ServiceCanadaCentre.addressLabel")}
+          hint={t("ServiceCanadaCentre.addressHint")}
+        />
+
+        <GcdsInput
+          inputId="province-input"
+          label={t("ServiceCanadaCentre.proviceLabel")}
+        />
+      </form>
+
+      <GcdsGrid columns="max-content max-content" gap="200">
+        <GcdsButton
+          type="button"
+          onClick={() => {
+            navigate(serviceCanadaCodePage);
+          }}
         >
-          {
-            //TODO: populate with real URL once available
-          }
-          <GcdsLink href={"#"} external={true}>
-            {t("ServiceCanadaCentre.learnMoreLink")}
-          </GcdsLink>
-        </GcdsNotice>
+          {t("ServiceCanadaCentre.continueButton")}
+        </GcdsButton>
+        <GcdsButton
+          type="button"
+          buttonRole="secondary"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          {t("ServiceCanadaCentre.backButton")}
+        </GcdsButton>
       </GcdsGrid>
+      <GcdsNotice
+        noticeRole="info"
+        noticeTitleTag="h2"
+        noticeTitle={t("ServiceCanadaCentre.moreInfoTitle")}
+      >
+        {
+          //TODO: populate with real URL once available
+        }
+        <GcdsLink href={"#"} external={true}>
+          {t("ServiceCanadaCentre.learnMoreLink")}
+        </GcdsLink>
+      </GcdsNotice>
     </GcdsContainer>
   );
 }
