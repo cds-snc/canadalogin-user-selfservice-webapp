8d77c00f58e3153c78c42623f3df22db700346da
Melody Habbouche <melody.habbouche@cds-snc.ca>
2026-06-19 19:43:17 +0000

    Add "Get Access to more services" Complete Identity Proofing notice

---
+++ frontend/src/features/IDV/components/CompleteIdentityProofingNotice.tsx
@@ -0,0 +1,28 @@
+import { GcdsButton, GcdsNotice, GcdsText } from "@gcds-core/components-react";
+import { useTranslation } from "react-i18next";
+import { useNavigate, useParams } from "react-router";
+
+export default function CompleteIdentityProofingNotice() {
+  const { language } = useParams<{ language: string }>();
+  const navigate = useNavigate();
+  const { t } = useTranslation("idv");
+  const startIdentityProofingPage = `/${language ?? "en"}/idv`;
+
+  return (
+    <GcdsNotice
+      noticeRole="info"
+      noticeTitleTag="h2"
+      noticeTitle={t("CompleteIdentityProofing.noticeTitle")}
+    >
+      <GcdsText>{t("CompleteIdentityProofing.noticeDescription")}</GcdsText>
+      <GcdsButton
+        type="button"
+        onGcdsClick={() => {
+          navigate(startIdentityProofingPage);
+        }}
+      >
+        {t("CompleteIdentityProofing.noticeCta")}
+      </GcdsButton>
+    </GcdsNotice>
+  );
+}
