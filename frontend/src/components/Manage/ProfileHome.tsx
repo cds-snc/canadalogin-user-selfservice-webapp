c91e491dfe77b81bb3fcb7e43d25ef001b8861ab
Melody Habbouche <melody.habbouche@cds-snc.ca>
2026-06-30 17:02:55 +0000

    Edit file

--- frontend/src/components/Manage/ProfileHome.tsx
+++ frontend/src/components/Manage/ProfileHome.tsx
@@ -3,7 +3,6 @@ import {
   GcdsHeading,
   GcdsGrid,
   GcdsText,
-  GcdsLink,
 } from "@gcds-core/components-react";
 
 import { useTranslation } from "react-i18next";
@@ -13,50 +12,9 @@ import VerifiedBadge from "../Badges/VerifiedBadge";
 import ViewContactPhoneNumber from "../../features/ContactPhoneNumber/components/ViewContactPhoneNumber";
 import ViewNameCard from "../../features/ProfileName/components/ViewProfileNameCard";
 import ViewLanguagePreferences from "../../features/LanguagePreference/components/ViewLanguagePreference";
+import ViewEmailInfo from "../../features/ProfileName/components/ViewEmailInfo";
 import ProvenInformationCard from "../../features/IDV/ProvenInformationCard";
 import IdentityInfoSuccessNotice from "../../features/IDV/IdentityInfoSuccessNotice";
-
-interface DisplayEmailInfoProps {
-  email: string;
-}
-
-type GcdsNavigationEvent = CustomEvent<string> & {
-  preventDefault: () => void;
-};
-
-const DisplayEmailInfo = ({ email }: DisplayEmailInfoProps) => {
-  const { language } = useParams();
-  const navigate = useNavigate();
-  const { t } = useTranslation("profile");
-  const editEmail = path(PAGES.editEmailPage, {
-    language,
-  });
-
-  return (
-    <>
-      <GcdsHeading tag="h3" marginTop="300">
-        {t("ProfileHome.email")}
-      </GcdsHeading>
-      <GcdsText>{t("ProfileHome.emailDescription")}</GcdsText>
-      <GcdsGrid columns="1fr auto" className="gridInline">
-        <GcdsText>{email}</GcdsText>
-        {DEV_ONLY_FEATURE && (
-          <GcdsLink
-            href={editEmail}
-            size="regular"
-            onGcdsClick={(event: GcdsNavigationEvent) => {
-              event.preventDefault();
-              navigate(event.detail);
-            }}
-          >
-            {t("ProfileHome.edit")}
-          </GcdsLink>
-        )}
-      </GcdsGrid>
-      <VerifiedBadge text={t("ProfileHome.verified")} />
-    </>
-  );
-};
 
 export default function ProfileHome({
   showIDVSuccessNotice = false,
