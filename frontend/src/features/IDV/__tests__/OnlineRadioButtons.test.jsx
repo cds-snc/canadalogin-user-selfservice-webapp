2a881af33335866a9f63312f9db0fee59d2b0393
amalungo <afonso.malungo@cds-snc.ca>
2026-07-10 11:15:02 -0400

    fix: Made minor update to test file

--- frontend/src/features/IDV/__tests__/OnlineRadioButtons.test.jsx
+++ frontend/src/features/IDV/__tests__/OnlineRadioButtons.test.jsx
@@ -73,7 +73,7 @@ describe("OnlineRadioButtons", () => {
     ).toBeInTheDocument();
     expect(
       screen.getByText(
-        /BC Service Card, Alberta\.ca account or Quebec.*prove your identity/,
+        /BC Service Card, Alberta\.ca account or Quebec to prove your identity/,
       ),
     ).toBeInTheDocument();
   });
