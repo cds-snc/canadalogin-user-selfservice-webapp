210e3333411eaa976bddedd8bf0a228502cfc697
amalungo <afonso.malungo@cds-snc.ca>
2026-07-15 13:44:35 -0400

    fix: Swapped GA ID o use non-dev google analytics ID (#1858)
    fix: Replace the GA ID with the non prod ID

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -7,7 +7,7 @@ interface AppConfig {
 
 const config: AppConfig = {
   apiUrl: import.meta.env.VITE_BACKEND_API_URL || "http://localhost:8000",
-  gatag: import.meta.env.VITE_GOOGLE_ANALYTICS_ID || "G-0Z1YGGZH02",
+  gatag: import.meta.env.VITE_GOOGLE_ANALYTICS_ID || "G-NQGLT9Y9GE",
   environment: import.meta.env.VITE_ENVIRONMENT || "dev",
   releaseTag: import.meta.env.VITE_RELEASE_TAG,
 };
