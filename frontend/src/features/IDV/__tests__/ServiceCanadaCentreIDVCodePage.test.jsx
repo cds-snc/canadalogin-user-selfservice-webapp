d46b303a7d2d88526498110d8df8dabb69a88cbd
amalungo <afonso.malungo@cds-snc.ca>
2026-07-24 10:13:02 -0400

    Removed text from test. No longer applicable.

--- frontend/src/features/IDV/__tests__/ServiceCanadaCentreIDVCodePage.test.jsx
+++ frontend/src/features/IDV/__tests__/ServiceCanadaCentreIDVCodePage.test.jsx
@@ -234,9 +234,6 @@ describe("ServiceCanadaCentreIDVCodePage", () => {
     expect(screen.getByText("Jane")).toBeInTheDocument();
     expect(screen.getByText("Doe")).toBeInTheDocument();
     expect(screen.getByText("1990-01-01")).toBeInTheDocument();
-    expect(
-      screen.getByText("Canadian and International Passport"),
-    ).toBeInTheDocument();
     expect(screen.queryByText("passport")).not.toBeInTheDocument();
     expect(screen.queryByText("Address")).not.toBeInTheDocument();
   });
@@ -264,9 +261,6 @@ describe("ServiceCanadaCentreIDVCodePage", () => {
     expect(screen.getByText("Jane")).toBeInTheDocument();
     expect(screen.getByText("Doe")).toBeInTheDocument();
     expect(screen.getByText(expectedDateOfBirth)).toBeInTheDocument();
-    expect(
-      screen.getByText("Canadian and International Passport"),
-    ).toBeInTheDocument();
     expect(screen.queryByText("Address")).not.toBeInTheDocument();
   });
 
