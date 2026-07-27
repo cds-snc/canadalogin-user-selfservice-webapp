a49a81f5158f8fdc1952ac01747723e4cdd2e995
mahamoud mohamoud <mahamoud.mohamoud@cds-snc.ca>
2026-07-27 14:19:11 +0000

    update

--- backend/app/auth/services/auth.py
+++ backend/app/auth/services/auth.py
@@ -209,4 +209,4 @@ async def reauthenticate_user(request: Request, returnToPage: str = "/"):
     # Use acr_values for step-up authentication to require LOA3 level
     return await oauth.verify.authorize_redirect(
         request, callback_redirect_uri, acr_values=acr_value
-        )
+    )
