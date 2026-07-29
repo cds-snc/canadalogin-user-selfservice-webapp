9eed7eafbbaeec2124c3cf662f4f3431a4f7d7ab
Cheyenne Boutilier <cheyenne.boutilier@cds-snc.ca>
2026-07-29 11:58:21 -0600

    refactor(identity-verification): [FE Content Update] Get ready to visit a Canada Post location - Remove "optional" (#1924)
    * refactor(identity-verification): [FE Content Update] Get ready to visit a Canada Post location - Remove "optional"
    
    * Fix prettier and errors in test
    
    * Removed comment

--- frontend/src/features/IDV/InPerson/validation/VisitCanadaPost.validation.ts
+++ frontend/src/features/IDV/InPerson/validation/VisitCanadaPost.validation.ts
@@ -76,14 +76,6 @@ export const getVisitCanadaPostValidation = (
     summaryErrorCodes.dateOfBirth = dateOfBirthValidationError;
   }
 
-  if (showAddressAndProvinceFields && !isNonEmptyTrimmed(formData.address)) {
-    summaryErrorCodes.address = "required";
-  }
-
-  if (showAddressAndProvinceFields && !isNonEmptyTrimmed(formData.province)) {
-    summaryErrorCodes.province = "required";
-  }
-
   return {
     isFormValid: Object.keys(summaryErrorCodes).length === 0,
     hasSelectedIdType,
