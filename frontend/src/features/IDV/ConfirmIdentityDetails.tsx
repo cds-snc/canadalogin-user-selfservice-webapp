e2c9546bc1767482c9b65c5e70c85102e4ee4d09
amalungo <afonso.malungo@cds-snc.ca>
2026-08-07 15:19:39 -0400

    fix(identity verification): Add French localizations for "Confirm what will be saved to your CanadaLogin"- #1945 (#1958)
    * Add French locals for 'Confirm what will be saved to your CanadaLogin'
    
    * Fix code style issue and text for failed test

--- frontend/src/features/IDV/ConfirmIdentityDetails.tsx
+++ frontend/src/features/IDV/ConfirmIdentityDetails.tsx
@@ -89,7 +89,9 @@ export default function ConfirmIdentityDetails() {
           </GcdsHeading>
 
           <GcdsText marginBottom="300">
-            <VerifiedBadge text={"Proven January 27, 2026"} />
+            <VerifiedBadge
+              text={t("ConfirmIdentityDetails.verifiedBadgeText")}
+            />
           </GcdsText>
 
           <ProvenInformationCard />
