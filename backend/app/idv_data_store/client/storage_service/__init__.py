fc63c719239586f1238c62d3969fdf822aa22e60
Sami rifi <sami.rifi@cds-snc.ca>
2026-08-12 14:00:32 -0400

    file cleanup structure

--- backend/app/idv_data_store/client/storage_service/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.idv_data_store.client.storage_service.identity_data_service import (
-    IdentityDataService,
-)
-
-__all__ = ["IdentityDataService"]
---
+++ backend/app/idv_data_store/services/__init__.py
@@ -0,0 +1,5 @@
+from app.idv_data_store.client.storage_service.identity_data_service import (
+    IdentityDataService,
+)
+
+__all__ = ["IdentityDataService"]
