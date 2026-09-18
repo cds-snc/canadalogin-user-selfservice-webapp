515f3c15375c2fdd958eb69c49692049e7fe846b
amalungo <afonso.malungo@cds-snc.ca>
2026-09-18 15:15:06 -0400

    Updated text and changed button to link

--- frontend/src/features/IDV/ProvenInformationCard.tsx
+++ frontend/src/features/IDV/ProvenInformationCard.tsx
@@ -1,5 +1,5 @@
 import {
-  GcdsButton,
+  GcdsLink,
   GcdsContainer,
   GcdsGrid,
   GcdsHeading,
@@ -74,20 +74,16 @@ export default function ProvenInformationCard({
         </GcdsText>
       </GcdsContainer>
     ) : null,
-    <GcdsGrid columns="1fr auto" className="gridInline">
-      <GcdsText marginBottom="300">
+    <GcdsContainer>
+      <GcdsText>
         {t("ProvenInformationCard.updateInfo")}
       </GcdsText>
-      <GcdsButton
-        buttonRole="secondary"
-        type="button"
-        onGcdsClick={() => {
-          navigate(startIdentityVerificationFlow);
-        }}
-      >
-        {t("ProvenInformationCard.updateButton")}
-      </GcdsButton>
-    </GcdsGrid>,
+      <GcdsText>
+        <GcdsLink href={startIdentityVerificationFlow}>
+          {t("ProvenInformationCard.updateButton")}
+        </GcdsLink>
+      </GcdsText>
+      </GcdsContainer>,
   ].filter(Boolean);
 
   return (
